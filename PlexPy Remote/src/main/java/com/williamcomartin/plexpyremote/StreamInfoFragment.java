4c51ed0431630245ae254379f731825dcc1d0748
William Comartin <wcomartin@fullcircletms.com>
2016-11-22 11:25:28 -0500

    Add Network Image View and Reference

--- PlexPy Remote/src/main/java/com/williamcomartin/plexpyremote/StreamInfoFragment.java
+++ PlexPy Remote/src/main/java/com/williamcomartin/plexpyremote/StreamInfoFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.toolbox.NetworkImageView;
 import com.williamcomartin.plexpyremote.Models.ActivityModels;
 
 
@@ -13,6 +14,7 @@ public class StreamInfoFragment extends Fragment {
 
 
     private ActivityModels.Activity mActivity;
+    private NetworkImageView vImage;
 
     public StreamInfoFragment() {
         // Required empty public constructor
@@ -24,7 +26,7 @@ public class StreamInfoFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_stream_info, container, false);
 
-
+        vImage = (NetworkImageView) view.findViewById(R.id.activity_stream_thumb);
 
         return view;
     }
