4f5967332739df22a0d358b077ff6d9bf8a867f3
Wreave <drivard100+github@gmail.com>
2016-12-05 19:12:41 -0800

    Added toggle for 'hit back twice to exit' and settings page cleanup

--- PlexPy Remote/src/main/java/com/williamcomartin/plexpyremote/ActivityActivity.java
+++ PlexPy Remote/src/main/java/com/williamcomartin/plexpyremote/ActivityActivity.java
@@ -220,25 +220,34 @@ public class ActivityActivity extends NavBaseActivity {
         myTimer.cancel();
     }
 
+
     @Override
+
     public void onBackPressed() {
-        if (mainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
-            super.onBackPressed();
-            return;
-        }
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed();
-            return;
-        }
+        Boolean backBoo = SP.getBoolean("app_settings_back", true);
+        if (backBoo) {
 
-        this.doubleBackToExitPressedOnce = true;
-        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                doubleBackToExitPressedOnce = false;
+            if (mainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+                super.onBackPressed();
+                return;
             }
-        }, 2000);
+            if (doubleBackToExitPressedOnce) {
+                super.onBackPressed();
+                return;
+            }
+
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 2000);
+        } else {
+            super.onBackPressed();
+            return;
+        }
     }
 }
