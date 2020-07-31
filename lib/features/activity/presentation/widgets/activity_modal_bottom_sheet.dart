2efccd822556f348c8a6a78e6f3ef092344757d3
TheMeanCanEHdian <drivard100@gmail.com>
2020-07-31 17:31:14 -0500

    Clean up ActivityModalBottomSheet widget tree.
    Removes excess IntrinsicHeight and aligns poster correctly.
    
    Fixes #23

--- lib/features/activity/presentation/widgets/activity_modal_bottom_sheet.dart
+++ lib/features/activity/presentation/widgets/activity_modal_bottom_sheet.dart
@@ -24,122 +24,146 @@ class ActivityModalBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
+        // Use LayoutBulilder to pass constraints to ActivityMediaDetails
         LayoutBuilder(
           builder: (BuildContext context, BoxConstraints constraints) {
             return IntrinsicHeight(
               child: Column(
                 children: <Widget>[
-                  // Creates a transparent area for the poster to hover over
-                  // Allows for that area to be tapped to dismiss the modal bottom sheet
-                  GestureDetector(
-                    onTap: () => Navigator.pop(context),
-                    child: Container(
-                      height: constraints.maxHeight * 0.02,
-                      width: constraints.maxWidth,
-                      color: Colors.transparent,
-                    ),
-                  ),
-                  // The main area of the modal bottom sheet
-                  ClipRRect(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
-                    child: IntrinsicHeight(
-                      child: Column(
+                  Stack(
+                    children: <Widget>[
+                      Column(
                         children: <Widget>[
-                          //* Activity art section
-                          Container(
-                            height: 130,
-                            width: constraints.maxWidth,
-                            child: Stack(
-                              children: <Widget>[
-                                Container(
-                                  width: constraints.maxWidth,
-                                  child: BackgroundImageChooser(
-                                    activity: activity,
-                                  ),
-                                ),
-                                BackdropFilter(
-                                  filter: ImageFilter.blur(
-                                    sigmaX: 8,
-                                    sigmaY: 8,
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Expanded(
-                                        child: Padding(
-                                          padding: const EdgeInsets.only(
-                                            top: 4,
-                                            left: 98,
-                                            bottom: 4,
-                                            right: 4,
-                                          ),
-                                          child: ActivityMediaInfo(
-                                            activity: activity,
-                                          ),
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                          right: 4,
-                                          left: 98,
-                                          top: 4,
-                                          bottom: 4,
-                                        ),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: <Widget>[
-                                            //* User name
-                                            Text(activity.friendlyName),
-                                            //* Time left or Live tv channel
-                                            activity.live == 0 &&
-                                                    activity.duration != null
-                                                ? TimeTotal(
-                                                    viewOffset:
-                                                        activity.viewOffset,
-                                                    duration: activity.duration,
-                                                  )
-                                                : activity.live == 1
-                                                    ? Text(
-                                                        '${activity.channelCallSign} ${activity.channelIdentifier}')
-                                                    : SizedBox(),
-                                          ],
-                                        ),
-                                      ),
-                                      activity.mediaType == 'photo' ||
-                                              activity.live == 1
-                                          ? ProgressBar(
-                                              progress: 100,
-                                              transcodeProgress: 0,
-                                            )
-                                          : ProgressBar(
-                                              progress:
-                                                  activity.progressPercent,
-                                              transcodeProgress:
-                                                  activity.transcodeProgress,
-                                            ),
-                                    ],
-                                  ),
-                                ),
-                              ],
+                          // Creates a transparent area for the poster to hover over
+                          // Allows for that area to be tapped to dismiss the modal bottom sheet
+                          GestureDetector(
+                            onTap: () => Navigator.pop(context),
+                            child: Container(
+                              height: 30,
+                              color: Colors.transparent,
                             ),
                           ),
-                          //* Activity details section
-                          Expanded(
+                          //* Activity art section
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10),
+                            ),
                             child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 15),
-                              color: PlexColorPalette.river_bed,
-                              child: ActivityMediaDetails(
-                                constraints: constraints,
-                                activity: activity,
+                              height: 130,
+                              child: Stack(
+                                children: <Widget>[
+                                  Container(
+                                    child: BackgroundImageChooser(
+                                      activity: activity,
+                                    ),
+                                  ),
+                                  BackdropFilter(
+                                    filter: ImageFilter.blur(
+                                      sigmaX: 8,
+                                      sigmaY: 8,
+                                    ),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Expanded(
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                              top: 4,
+                                              left: 98,
+                                              bottom: 4,
+                                              right: 4,
+                                            ),
+                                            child: ActivityMediaInfo(
+                                              activity: activity,
+                                            ),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                            right: 4,
+                                            left: 98,
+                                            top: 4,
+                                            bottom: 4,
+                                          ),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: <Widget>[
+                                              //* User name
+                                              Text(activity.friendlyName),
+                                              //* Time left or Live tv channel
+                                              activity.live == 0 &&
+                                                      activity.duration != null
+                                                  ? TimeTotal(
+                                                      viewOffset:
+                                                          activity.viewOffset,
+                                                      duration:
+                                                          activity.duration,
+                                                    )
+                                                  : activity.live == 1
+                                                      ? Text(
+                                                          '${activity.channelCallSign} ${activity.channelIdentifier}')
+                                                      : SizedBox(),
+                                            ],
+                                          ),
+                                        ),
+                                        //* Progress bar
+                                        activity.mediaType == 'photo' ||
+                                                activity.live == 1
+                                            ? ProgressBar(
+                                                progress: 100,
+                                                transcodeProgress: 0,
+                                              )
+                                            : ProgressBar(
+                                                progress:
+                                                    activity.progressPercent,
+                                                transcodeProgress:
+                                                    activity.transcodeProgress,
+                                              ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
                         ],
+                      ),
+                      //* Poster
+                      Positioned(
+                        bottom: 14,
+                        child: Container(
+                          height: 130,
+                          padding: EdgeInsets.only(
+                            left: 4,
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(4),
+                            child: Stack(
+                              children: <Widget>[
+                                PosterChooser(
+                                  activity: activity,
+                                ),
+                                if (activity.state == 'paused' ||
+                                    activity.state == 'buffering')
+                                  StatusPosterOverlay(state: activity.state),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  //* Activity details section
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      color: PlexColorPalette.river_bed,
+                      child: ActivityMediaDetails(
+                        constraints: constraints,
+                        activity: activity,
                       ),
                     ),
                   ),
@@ -147,25 +171,6 @@ class ActivityModalBottomSheet extends StatelessWidget {
               ),
             );
           },
-        ),
-        // Poster
-        Container(
-          height: 130,
-          padding: EdgeInsets.only(
-            left: 4,
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(4),
-            child: Stack(
-              children: <Widget>[
-                PosterChooser(
-                  activity: activity,
-                ),
-                if (activity.state == 'paused' || activity.state == 'buffering')
-                  StatusPosterOverlay(state: activity.state),
-              ],
-            ),
-          ),
         ),
       ],
     );
