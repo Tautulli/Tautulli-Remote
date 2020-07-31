ed922a7e7923217f97032f70cc4528ff9ea4d59d
TheMeanCanEHdian <drivard100@gmail.com>
2020-07-30 19:49:13 -0500

    Change system for connecting to Tautulli API.

---
+++ lib/core/api/tautulli_api.dart
@@ -0,0 +1,281 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
+import 'package:quiver/strings.dart';
+
+import '../../features/settings/domain/usecases/settings.dart';
+import '../database/domain/entities/server.dart';
+import '../error/exception.dart';
+
+abstract class TautulliApi {
+  Future connectionHandler();
+  Future fetchTautulli();
+  Future<Map<String, dynamic>> getActivity(String tautulliId);
+  Future<Map<String, dynamic>> getGeoipLookup({
+    @required String tautulliId,
+    String ipAddress,
+  });
+  Future<String> pmsImageProxy({
+    @required String tautulliId,
+    String img,
+    int ratingKey,
+    int width,
+    int height = 300,
+    int opacity,
+    int background,
+    int blur,
+    String fallback,
+  });
+  Future<Map<String, dynamic>> registerDevice({
+    @required String connectionProtocol,
+    @required String connectionDomain,
+    @required String connectionPath,
+    @required String deviceToken,
+    @required String deviceId,
+    @required String deviceName,
+    @required String onesignalId,
+  });
+}
+
+class TautulliApiImpl implements TautulliApi {
+  final http.Client client;
+  final Settings settings;
+
+  TautulliApiImpl({
+    @required this.client,
+    @required this.settings,
+  });
+
+  /// Handles failing over to the secondary connection if needed.
+  /// Should not be called directly.
+  Future connectionHandler({
+    String tautulliId,
+    String primaryConnectionProtocol,
+    String primaryConnectionDomain,
+    String primaryConnectionPath,
+    String deviceToken,
+    @required String cmd,
+    Map<String, String> params,
+  }) async {
+    String secondaryConnectionAddress;
+    String secondaryConnectionProtocol;
+    String secondaryConnectionDomain;
+    String secondaryConnectionPath;
+
+    // If tautulliId is provided then query for existing server info
+    if (tautulliId != null) {
+      final Server server = await settings.getServerByTautulliId(tautulliId);
+      primaryConnectionProtocol = server.primaryConnectionProtocol;
+      primaryConnectionDomain = server.primaryConnectionDomain;
+      primaryConnectionPath = server.primaryConnectionPath;
+      secondaryConnectionAddress = server.secondaryConnectionAddress;
+      secondaryConnectionProtocol = server.secondaryConnectionProtocol;
+      secondaryConnectionDomain = server.secondaryConnectionDomain;
+      secondaryConnectionPath = server.secondaryConnectionPath;
+      deviceToken = server.deviceToken;
+
+      // Verify server has primaryConnectionAddress and deviceToken
+      if (isEmpty(server.primaryConnectionAddress) ||
+          isEmpty(server.deviceToken)) {
+        throw SettingsException();
+      }
+    }
+    // If no tautulliId and any primaryConnection options
+    // are missing throw SettingsException.
+    else if (isEmpty(primaryConnectionProtocol) ||
+        isEmpty(primaryConnectionDomain) ||
+        isEmpty(deviceToken)) {
+      throw SettingsException();
+    }
+
+    var response;
+
+    // Attempt to connect using primary connection
+    try {
+      response = await fetchTautulli(
+        connectionProtocol: primaryConnectionProtocol,
+        connectionDomain: primaryConnectionDomain,
+        connectionPath: primaryConnectionPath,
+        deviceToken: deviceToken,
+        cmd: cmd,
+        params: params,
+      );
+    } catch (error) {
+      // If secondary connection configured try again with that address
+      if (isNotEmpty(secondaryConnectionAddress)) {
+        //? Does this need to be caught if we want the error to actually be thrown
+        //? Probably not
+        response = await fetchTautulli(
+          connectionProtocol: secondaryConnectionProtocol,
+          connectionDomain: secondaryConnectionDomain,
+          connectionPath: secondaryConnectionPath,
+          deviceToken: deviceToken,
+          cmd: cmd,
+          params: params,
+        );
+      } else {
+        // Re-throw caught error if no secondary connection
+        throw error;
+      }
+    }
+    return response;
+  }
+
+  /// Default constructor for Tautulli API endpoints.
+  /// Should not be called directly.
+  Future fetchTautulli({
+    @required String connectionProtocol,
+    @required String connectionDomain,
+    @required String connectionPath,
+    @required String deviceToken,
+    @required String cmd,
+    Map<String, String> params,
+  }) async {
+    // If no params provided initalize params
+    if (params == null) {
+      params = {};
+    }
+    params['cmd'] = cmd;
+    params['apikey'] = deviceToken;
+    params['app'] = 'true';
+
+    // Construct URI based on connection protocol using the connection domain/path as well as the params
+    Uri uri;
+    switch (connectionProtocol) {
+      case ('http'):
+        uri = Uri.http('$connectionDomain', '$connectionPath/api/v2', params);
+        break;
+      case ('https'):
+        uri = Uri.https('$connectionDomain', '$connectionPath/api/v2', params);
+    }
+
+    //* Return URI for pmsImageProxy if URI is good
+    if (cmd == 'pms_image_proxy') {
+      final uriCheck = await client.head(uri);
+      if (uriCheck.statusCode != 200) {
+        throw ServerException();
+      }
+      return uri;
+    }
+
+    // Call API using constructed URI
+    final response = await client.get(
+      uri,
+      headers: {'Content-Type': 'application/json'},
+    ).timeout(Duration(seconds: 5));
+
+    if (response.statusCode != 200) {
+      throw ServerException();
+    }
+    return response;
+  }
+
+  /// Returns a Map of the decoded JSON response from
+  /// the `get_activity` endpoint.
+  ///
+  /// Throws a [JsonDecodeException] if the json decode fails.
+  Future<Map<String, dynamic>> getActivity(String tautulliId) async {
+    final response = await connectionHandler(
+      tautulliId: tautulliId,
+      cmd: 'get_activity',
+    );
+
+    // Attempt to parse reponse into JSON
+    Map<String, dynamic> responseJson;
+    try {
+      responseJson = json.decode(response.body);
+    } catch (_) {
+      throw JsonDecodeException();
+    }
+
+    return responseJson;
+  }
+
+  /// Returns a Map of the decoded JSON response from
+  /// the `get_geoip_lookup` endpoint.
+  ///
+  /// Throws a [JsonDecodeException] if the json decode fails.
+  Future<Map<String, dynamic>> getGeoipLookup({
+    @required String tautulliId,
+    @required String ipAddress,
+  }) async {
+    final response = await connectionHandler(
+        tautulliId: tautulliId,
+        cmd: 'get_geoip_lookup',
+        params: {'ip_address': ipAddress});
+
+    // Attempt to parse reponse into JSON
+    Map<String, dynamic> responseJson;
+    try {
+      responseJson = json.decode(response.body);
+    } catch (_) {
+      throw JsonDecodeException();
+    }
+
+    return responseJson;
+  }
+
+  /// Returns a constructed URL as a String for accessing an
+  /// image via the `pms_image_proxy` endpoint.
+  Future<String> pmsImageProxy({
+    @required String tautulliId,
+    String img,
+    int ratingKey,
+    int width,
+    int height = 300,
+    int opacity,
+    int background,
+    int blur,
+    String fallback,
+  }) async {
+    final Uri uri = await connectionHandler(
+      tautulliId: tautulliId,
+      cmd: 'pms_image_proxy',
+      params: {
+        'img': img,
+        'rating_key': ratingKey.toString(),
+        'width': width.toString(),
+        'height': height.toString(),
+        'opacity': opacity.toString(),
+        'background': background.toString(),
+        'blur': blur.toString(),
+        'fallback': fallback,
+      },
+    );
+    return uri.toString();
+  }
+
+  Future<Map<String, dynamic>> registerDevice({
+    @required String connectionProtocol,
+    @required String connectionDomain,
+    @required String connectionPath,
+    @required String deviceToken,
+    @required String deviceId,
+    @required String deviceName,
+    @required String onesignalId,
+  }) async {
+    final response = await connectionHandler(
+      primaryConnectionProtocol: connectionProtocol,
+      primaryConnectionDomain: connectionDomain,
+      primaryConnectionPath: connectionPath,
+      deviceToken: deviceToken,
+      cmd: 'register_device',
+      params: {
+        'device_name': deviceName,
+        'device_id': deviceId,
+        'onesignal_id': onesignalId,
+      },
+    );
+
+    // Attempt to parse reponse into JSON
+    Map<String, dynamic> responseJson;
+    try {
+      responseJson = json.decode(response.body);
+    } catch (_) {
+      throw JsonDecodeException();
+    }
+
+    return responseJson;
+  }
+}
---
+++ test/core/api/tautulli_api_test.dart
@@ -0,0 +1,295 @@
+import 'dart:async';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+import 'package:tautulli_remote_tdd/core/api/tautulli_api.dart';
+import 'package:tautulli_remote_tdd/core/database/data/models/server_model.dart';
+import 'package:tautulli_remote_tdd/core/error/exception.dart';
+import 'package:tautulli_remote_tdd/features/settings/domain/usecases/settings.dart';
+import 'package:matcher/matcher.dart';
+
+import '../../fixtures/fixture_reader.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+class MockSettings extends Mock implements Settings {}
+
+void main() {
+  TautulliApiImpl tautulliApi;
+  MockHttpClient mockHttpClient;
+  MockSettings mockSettings;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    mockSettings = MockSettings();
+    tautulliApi = TautulliApiImpl(
+      client: mockHttpClient,
+      settings: mockSettings,
+    );
+  });
+
+  final tTautulliId = 'jkl';
+
+  final tServerModel = ServerModel(
+    primaryConnectionAddress: 'http://tautulli.com',
+    primaryConnectionProtocol: 'http',
+    primaryConnectionDomain: 'tautulli.com',
+    primaryConnectionPath: null,
+    secondaryConnectionAddress: 'https://plexpy.com',
+    secondaryConnectionProtocol: 'https',
+    secondaryConnectionDomain: 'plexpy.com',
+    secondaryConnectionPath: null,
+    deviceToken: 'abc',
+    tautulliId: 'jkl',
+    plexName: 'Plex',
+  );
+
+  group('connectionHandler', () {
+    test(
+      'should fetch the server from settings by tautulliId',
+      () async {
+        // arrange
+        when(mockSettings.getServerByTautulliId(any))
+            .thenAnswer((_) async => tServerModel);
+        when(
+          mockHttpClient.get(
+            any,
+            headers: anyNamed('headers'),
+          ),
+        ).thenAnswer((_) async => http.Response(fixture('activity.json'), 200));
+        // act
+        await tautulliApi.connectionHandler(
+          tautulliId: tTautulliId,
+          cmd: '',
+        );
+        // assert
+        verify(mockSettings.getServerByTautulliId(tTautulliId));
+      },
+    );
+
+    test(
+      'should throw SettingsException if primaryConnectionAddress is missing',
+      () async {
+        // arrange
+        final tBadServerModel = ServerModel(
+          primaryConnectionAddress: null,
+          primaryConnectionProtocol: null,
+          primaryConnectionDomain: null,
+          primaryConnectionPath: null,
+          deviceToken: 'abc',
+          plexName: null,
+          tautulliId: null,
+        );
+        when(mockSettings.getServerByTautulliId(any))
+            .thenAnswer((_) async => tBadServerModel);
+        // act
+        final call = tautulliApi.connectionHandler;
+        // assert
+        expect(
+            () async => call(
+                  tautulliId: tTautulliId,
+                  cmd: '',
+                ),
+            throwsA(TypeMatcher<SettingsException>()));
+      },
+    );
+
+    test(
+      'should throw SettingsException if deviceToken is missing',
+      () async {
+        // arrange
+        final tBadServerModel = ServerModel(
+          primaryConnectionAddress: 'http://tautulli.com',
+          primaryConnectionProtocol: null,
+          primaryConnectionDomain: null,
+          primaryConnectionPath: null,
+          deviceToken: null,
+          plexName: null,
+          tautulliId: null,
+        );
+        when(mockSettings.getServerByTautulliId(any))
+            .thenAnswer((_) async => tBadServerModel);
+        // act
+        final call = tautulliApi.connectionHandler;
+        // assert
+        expect(
+          () async => call(
+            tautulliId: tTautulliId,
+            cmd: '',
+          ),
+          throwsA(TypeMatcher<SettingsException>()),
+        );
+      },
+    );
+  });
+
+  group('fetchTautulli', () {
+    test(
+      'should perform a GET on address with application/json header',
+      () async {
+        // arrange
+        when(mockSettings.getServerByTautulliId(any))
+            .thenAnswer((_) async => tServerModel);
+        when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => http.Response(fixture('activity.json'), 200));
+        // act
+        await tautulliApi.fetchTautulli(
+          connectionProtocol: tServerModel.primaryConnectionProtocol,
+          connectionDomain: tServerModel.primaryConnectionDomain,
+          connectionPath: tServerModel.primaryConnectionPath,
+          deviceToken: tServerModel.deviceToken,
+          cmd: '',
+        );
+        // assert
+        verify(
+          mockHttpClient.get(
+            any,
+            headers: {'Content-Type': 'application/json'},
+          ),
+        );
+      },
+    );
+
+    test(
+      'should use Uri.http if connection protocol is http',
+      () async {
+        // arrange
+        final uri = Uri.http(
+          '${tServerModel.primaryConnectionDomain}',
+          '${tServerModel.primaryConnectionPath}/api/v2',
+          {'cmd': '', 'apikey': tServerModel.deviceToken, 'app': 'true'},
+        );
+        when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => http.Response(fixture('activity.json'), 200));
+        // act
+        await tautulliApi.fetchTautulli(
+          connectionProtocol: tServerModel.primaryConnectionProtocol,
+          connectionDomain: tServerModel.primaryConnectionDomain,
+          connectionPath: tServerModel.primaryConnectionPath,
+          deviceToken: tServerModel.deviceToken,
+          cmd: '',
+        );
+        // assert
+        verify(mockHttpClient.get(uri, headers: anyNamed('headers')));
+      },
+    );
+
+    test(
+      'should use Uri.https if connection protocol is https',
+      () async {
+        // arrange
+        final uri = Uri.https(
+          '${tServerModel.secondaryConnectionDomain}',
+          '${tServerModel.secondaryConnectionPath}/api/v2',
+          {'cmd': '', 'apikey': tServerModel.deviceToken, 'app': 'true'},
+        );
+        when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => http.Response(fixture('activity.json'), 200));
+        // act
+        await tautulliApi.fetchTautulli(
+          connectionProtocol: tServerModel.secondaryConnectionProtocol,
+          connectionDomain: tServerModel.secondaryConnectionDomain,
+          connectionPath: tServerModel.secondaryConnectionPath,
+          deviceToken: tServerModel.deviceToken,
+          cmd: '',
+        );
+        // assert
+        verify(mockHttpClient.get(uri, headers: anyNamed('headers')));
+      },
+    );
+
+    test(
+      'should throw TimeoutException if GET request takes too long',
+      () async {
+        // arrange
+        when(mockSettings.getServerByTautulliId(any))
+            .thenAnswer((_) async => tServerModel);
+        when(mockHttpClient.get(any, headers: anyNamed('headers')))
+            .thenAnswer((_) => Future.delayed(Duration(seconds: 10)));
+        // act
+        final call = tautulliApi.fetchTautulli;
+        // assert
+        expect(
+          () async => call(
+            connectionProtocol: tServerModel.primaryConnectionProtocol,
+            connectionDomain: tServerModel.primaryConnectionDomain,
+            connectionPath: tServerModel.primaryConnectionPath,
+            deviceToken: tServerModel.deviceToken,
+            cmd: '',
+          ),
+          throwsA(TypeMatcher<TimeoutException>()),
+        );
+      },
+    );
+
+    test(
+      'should throw a ServerException if the response status code is not 200',
+      () async {
+        // arrange
+        when(mockHttpClient.get(any, headers: anyNamed('headers')))
+            .thenAnswer((_) async => http.Response('is bad', 404));
+        // act
+        final call = tautulliApi.fetchTautulli;
+        // assert
+        expect(
+          () async => call(
+            connectionProtocol: tServerModel.primaryConnectionProtocol,
+            connectionDomain: tServerModel.primaryConnectionDomain,
+            connectionPath: tServerModel.primaryConnectionPath,
+            deviceToken: tServerModel.deviceToken,
+            cmd: '',
+          ),
+          throwsA(TypeMatcher<ServerException>()),
+        );
+      },
+    );
+
+    test(
+      'should return a JSON map if GET request returns valid data',
+      () async {
+        // arrange
+        when(mockSettings.getServerByTautulliId(any))
+            .thenAnswer((_) async => tServerModel);
+        when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => http.Response(fixture('activity.json'), 200));
+        // act
+        final response = await tautulliApi.fetchTautulli(
+          connectionProtocol: tServerModel.primaryConnectionProtocol,
+          connectionDomain: tServerModel.primaryConnectionDomain,
+          connectionPath: tServerModel.primaryConnectionPath,
+          deviceToken: tServerModel.deviceToken,
+          cmd: '',
+        );
+        // assert
+        expect(response, TypeMatcher<http.Response>());
+      },
+    );
+  });
+
+  // group('getActivity', () {
+  //   test(
+  //     'should throw a JsonDecodeException the response is unable to be decoded into JSON',
+  //     () async {
+  //       // arrange
+  //       when(mockSettings.getServerByTautulliId(any))
+  //           .thenAnswer((_) async => tServerModel);
+  //       when(mockHttpClient.get(any, headers: anyNamed('headers')))
+  //           .thenAnswer((_) async => http.Response('<!doctype html>', 200));
+  //       // act
+  //       final call = tautulliApi.fetchTautulli;
+  //       // assert
+  //       expect(
+  //         () async => call(
+  //           connectionProtocol: tServerModel.primaryConnectionProtocol,
+  //           connectionDomain: tServerModel.primaryConnectionDomain,
+  //           connectionPath: tServerModel.primaryConnectionPath,
+  //           deviceToken: tServerModel.deviceToken,
+  //           cmd: '',
+  //         ),
+  //         throwsA(TypeMatcher<JsonDecodeException>()),
+  //       );
+  //     },
+  //   );
+  // });
+}
