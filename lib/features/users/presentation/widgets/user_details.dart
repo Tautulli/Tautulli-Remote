722fca0b3f4660a2159d27797160983ecba13c0e
TheMeanCanEHdian <drivard100@gmail.com>
2020-10-07 15:30:04 -0500

    Change users last played to total plays/duration.
    Implements #38

--- lib/features/users/presentation/widgets/user_details.dart
+++ lib/features/users/presentation/widgets/user_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/helpers/color_palette_helper.dart';
 import '../../../../core/helpers/time_format_helper.dart';
 import '../../domain/entities/user_table.dart';
 
@@ -22,17 +23,7 @@ class UsersDetails extends StatelessWidget {
           overflow: TextOverflow.ellipsis,
           style: TextStyle(fontSize: 18),
         ),
-        user.lastPlayed != null
-            ? Text(
-                '${user.lastPlayed}',
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(fontSize: 15),
-              )
-            : Text(
-                '',
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(fontSize: 15),
-              ),
+        _playsAndDuration(),
         user.lastSeen != null
             ? Text(
                 '${TimeFormatHelper.timeAgo(user.lastSeen)}',
@@ -49,6 +40,114 @@ class UsersDetails extends StatelessWidget {
                 ),
               ),
       ],
+    );
+  }
+
+  RichText _playsAndDuration() {
+    Map<String, int> durationMap =
+        TimeFormatHelper.durationMap(Duration(seconds: user.duration));
+
+    return RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'PLAYS ',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              color: TautulliColorPalette.not_white,
+            ),
+          ),
+          TextSpan(
+            text: user.plays.toString(),
+            style: TextStyle(
+              color: PlexColorPalette.gamboge,
+            ),
+          ),
+          TextSpan(text: '   '),
+          if (durationMap['day'] > 1 ||
+              durationMap['hour'] > 1 ||
+              durationMap['min'] > 1 ||
+              durationMap['sec'] > 1)
+            TextSpan(
+              text: 'DURATION ',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: TautulliColorPalette.not_white,
+              ),
+            ),
+          if (durationMap['day'] > 0)
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: durationMap['day'].toString(),
+                  style: TextStyle(
+                    color: PlexColorPalette.gamboge,
+                  ),
+                ),
+                TextSpan(
+                  text: ' days ',
+                  style: TextStyle(
+                    color: TautulliColorPalette.not_white,
+                  ),
+                ),
+              ],
+            ),
+          if (durationMap['hour'] > 0)
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: durationMap['hour'].toString(),
+                  style: TextStyle(
+                    color: PlexColorPalette.gamboge,
+                  ),
+                ),
+                TextSpan(
+                  text: ' hrs ',
+                  style: TextStyle(
+                    color: TautulliColorPalette.not_white,
+                  ),
+                ),
+              ],
+            ),
+          if (durationMap['min'] > 0)
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: durationMap['min'].toString(),
+                  style: TextStyle(
+                    color: PlexColorPalette.gamboge,
+                  ),
+                ),
+                TextSpan(
+                  text: ' mins',
+                  style: TextStyle(
+                    color: TautulliColorPalette.not_white,
+                  ),
+                ),
+              ],
+            ),
+          if (durationMap['day'] < 1 &&
+              durationMap['hour'] < 1 &&
+              durationMap['min'] < 1 &&
+              durationMap['sec'] > 0)
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: durationMap['sec'].toString(),
+                  style: TextStyle(
+                    color: PlexColorPalette.gamboge,
+                  ),
+                ),
+                TextSpan(
+                  text: ' secs',
+                  style: TextStyle(
+                    color: TautulliColorPalette.not_white,
+                  ),
+                ),
+              ],
+            ),
+        ],
+      ),
     );
   }
 }
