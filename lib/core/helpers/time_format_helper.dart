f79fa8e5563d8918d227b4399c4613ffbfbc9511
TheMeanCanEHdian <drivard100@gmail.com>
2020-10-23 09:33:11 -0500

    Accept date and time formats in cleanDateTime.

--- lib/core/helpers/time_format_helper.dart
+++ lib/core/helpers/time_format_helper.dart
@@ -76,11 +76,20 @@ class TimeFormatHelper {
     return 'ETA: ${DateFormat.jm().format(eta)}';
   }
 
-  static String cleanDateTime(int timeSinceEpochInSeconds) {
+  static String cleanDateTime(
+    int timeSinceEpochInSeconds, {
+    String dateFormat,
+    String timeFormat,
+  }) {
     DateTime dateTime =
         DateTime.fromMillisecondsSinceEpoch(timeSinceEpochInSeconds * 1000);
 
-    return '${dateTime.year}-${dateTime.month.toString().padLeft(2, "0")}-${dateTime.day.toString().padLeft(2, "0")} ${dateTime.hour}:${dateTime.minute.toString().padLeft(2, "0")}';
+    final String parsedDateFormat =
+        dateFormat != null ? _parseDateFormat(dateFormat) : 'y-MM-dd';
+    final String parsedTimeFormat =
+        timeFormat != null ? _parseTimeFormat(timeFormat) : 'HH:mm';
+
+    return DateFormat('$parsedDateFormat $parsedTimeFormat').format(dateTime);
   }
 
   static Map<String, int> durationMap(Duration duration) {
@@ -91,4 +100,44 @@ class TimeFormatHelper {
       'sec': duration.inSeconds.remainder(60),
     };
   }
+}
+
+String _parseDateFormat(String dateFormat) {
+  return dateFormat
+          .replaceAll('YYYY', 'y')
+          .replaceAll('YY', 'yy')
+          // .replaceAll('MMMM', 'MMMM')
+          // .replaceAll('MMM', 'MMM')
+          // .replaceAll('MM', 'MM')
+          // .replaceAll('M', 'M')
+          .replaceAll('Mo', 'M')
+          .replaceAll('dddd', 'EEEE')
+          .replaceAll('ddd', 'E')
+          .replaceAll('dd', 'E')
+          .replaceAll('d', 'E')
+          .replaceAll('do', 'E')
+          .replaceAll('DDDD', 'DDD')
+          .replaceAll('DDD', 'DD')
+          .replaceAll('DDDo', 'DD')
+          .replaceAll('DD', 'dd')
+          .replaceAll('D', 'd')
+          .replaceAll('Do', 'd')
+      ;
+}
+
+String _parseTimeFormat(String timeFormat) {
+  return timeFormat
+      // .replaceAll('HH', 'HH')
+      // .replaceAll('H', 'H')
+      // .replaceAll('hh', 'hh')
+      // .replaceAll('h', 'h')
+      // .replaceAll('mm', 'mm')
+      // .replaceAll('m', 'm')
+      // .replaceAll('ss', 'ss')
+      // .replaceAll('s', 's')
+      .replaceAll('a', 'a')
+      .replaceAll('A', 'a')
+      .replaceAll('ZZ', '')
+      .replaceAll('Z', '')
+      .replaceAll('X', '');
 }
