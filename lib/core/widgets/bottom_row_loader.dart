a7db5e3aee25bcda3a8f87b01d7aea5f4eb8e190
TheMeanCanEHdian <drivard100@gmail.com>
2021-02-05 11:49:00 -0500

    Add BottomRowLoader widget.

---
+++ lib/core/widgets/bottom_row_loader.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BottomRowLoader extends StatelessWidget {
+  final int index;
+
+  const BottomRowLoader({
+    @required this.index,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      decoration: BoxDecoration(
+        color: index % 2 == 0 ? Colors.black26 : null,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8.0,
+          vertical: 16,
+        ),
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
