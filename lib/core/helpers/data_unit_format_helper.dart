fd6049ec3425a29a074104230d6a5443738b4644
TheMeanCanEHdian <drivard100@gmail.com>
2021-02-15 11:41:18 -0500

    Add bitrate data unit helper.

--- lib/core/helpers/data_unit_format_helper.dart
+++ lib/core/helpers/data_unit_format_helper.dart
@@ -4,4 +4,12 @@ class DataUnitFormatHelper {
   static String prettyFilesize(int bytes) {
     return filesize(bytes, 1);
   }
+
+  static String bitrate(int kiloBitsPerSecond) {
+    if (kiloBitsPerSecond < 1000) {
+      return '$kiloBitsPerSecond Kbps';
+    }
+
+    return '${kiloBitsPerSecond / 1000} Mbps';
+  }
 }
