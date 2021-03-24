5d062585d15d5fc0db9edf1f064a67de66ca3b89
TheMeanCanEHdian <drivard100@gmail.com>
2021-03-24 16:47:06 -0400

    Switch to UserTable object in UserIcon.

--- lib/core/widgets/user_icon.dart
+++ lib/core/widgets/user_icon.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../features/users/domain/entities/user_table.dart';
 import '../helpers/color_palette_helper.dart';
 
 class UserIcon extends StatelessWidget {
-  final int isActive;
-  final bool hasNetworkImage;
-  final AsyncSnapshot<dynamic> snapshot;
+  final UserTable user;
   final bool maskSensitiveInfo;
 
   const UserIcon({
     Key key,
-    @required this.isActive,
-    @required this.hasNetworkImage,
-    @required this.snapshot,
+    @required this.user,
     @required this.maskSensitiveInfo,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final bool hasNetworkImage =
+        user.userThumb != null ? user.userThumb.startsWith('http') : false;
+
     return Stack(
       children: [
         Container(
@@ -27,9 +27,9 @@ class UserIcon extends StatelessWidget {
           decoration: BoxDecoration(
             image: DecorationImage(
               image: (hasNetworkImage &&
-                      snapshot.data != null &&
+                      user.userThumb != null &&
                       !maskSensitiveInfo)
-                  ? snapshot.data['image']
+                  ? NetworkImage(user.userThumb)
                   : AssetImage('assets/images/default_profile.png'),
               fit: BoxFit.cover,
             ),
@@ -38,7 +38,7 @@ class UserIcon extends StatelessWidget {
             ),
             border: Border.all(
               color: (hasNetworkImage &&
-                      snapshot.data != null &&
+                      user.userThumb != null &&
                       !maskSensitiveInfo)
                   ? Colors.transparent
                   : Color.fromRGBO(69, 69, 69, 1),
@@ -46,7 +46,7 @@ class UserIcon extends StatelessWidget {
             ),
           ),
         ),
-        if (isActive == 0)
+        if (user.isActive == 0)
           Positioned(
             bottom: 0,
             right: 0,
