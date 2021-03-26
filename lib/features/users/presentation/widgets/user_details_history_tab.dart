e8d097910212f9b97b2ede905636858bc763bf0e
TheMeanCanEHdian <drivard100@gmail.com>
2021-03-25 23:41:28 -0400

    Disable user button for user details history tab.

--- lib/features/users/presentation/widgets/user_details_history_tab.dart
+++ lib/features/users/presentation/widgets/user_details_history_tab.dart
@@ -157,6 +157,7 @@ class _UserDetailsHistoryTabContentState
                                                   is SettingsLoadSuccess
                                               ? settingsState.maskSensitiveInfo
                                               : false,
+                                          disableUserButton: true,
                                         );
                                       },
                                     ),
