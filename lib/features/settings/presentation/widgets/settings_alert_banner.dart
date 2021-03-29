728e77b648c0143ab7f97649d5551d0e6855bdfe
TheMeanCanEHdian <drivard100@gmail.com>
2021-03-29 10:42:25 -0400

    Do not show consent banner if dismissed.

--- lib/features/settings/presentation/widgets/settings_alert_banner.dart
+++ lib/features/settings/presentation/widgets/settings_alert_banner.dart
@@ -7,6 +7,7 @@ import '../../../../core/helpers/color_palette_helper.dart';
 import '../../../onesignal/presentation/bloc/onesignal_health_bloc.dart';
 import '../../../onesignal/presentation/bloc/onesignal_privacy_bloc.dart';
 import '../../../onesignal/presentation/bloc/onesignal_subscription_bloc.dart';
+import '../bloc/settings_bloc.dart';
 
 class SettingsAlertBanner extends StatelessWidget {
   const SettingsAlertBanner({Key key}) : super(key: key);
@@ -14,73 +15,91 @@ class SettingsAlertBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final oneSignalPrivacyBloc = context.read<OneSignalPrivacyBloc>();
+    final settingsBloc = context.read<SettingsBloc>();
 
-    return BlocBuilder<OneSignalHealthBloc, OneSignalHealthState>(
-      builder: (context, healthState) {
-        return BlocBuilder<OneSignalSubscriptionBloc,
-            OneSignalSubscriptionState>(
-          builder: (context, subscriptionState) {
-            // Display alert banner about consenting to privacy policy
-            if (oneSignalPrivacyBloc.state is OneSignalPrivacyConsentFailure) {
-              if (subscriptionState is OneSignalSubscriptionFailure) {
-                return _AlertBanner(
-                  title: subscriptionState.title,
-                  message: Text(
-                    subscriptionState.message,
-                  ),
-                  buttonOne: TextButton(
-                    child: Text('VIEW PRIVACY PAGE'),
-                    onPressed: () =>
-                        Navigator.of(context).pushNamed('/privacy'),
-                  ),
-                  backgroundColor: Colors.deepOrange[900],
-                );
-              }
-            }
-            // Display alert banner about failure to connect to onesignal.com
-            if (oneSignalPrivacyBloc.state is OneSignalPrivacyConsentSuccess &&
-                healthState is OneSignalHealthFailure) {
-              return _AlertBanner(
-                title: 'Unable to reach OneSignal',
-                message: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('• Notifications will not work.'),
-                    Text('• Registration with OneSignal will fail.'),
-                  ],
-                ),
-                buttonOne: TextButton(
-                  child: Text('CHECK AGAIN'),
-                  onPressed: () => context
-                      .read<OneSignalHealthBloc>()
-                      .add(OneSignalHealthCheck()),
-                ),
-              );
-            }
-            // Display alert banner about waiting to subscribe to OneSignal
-            if (oneSignalPrivacyBloc.state is OneSignalPrivacyConsentSuccess) {
-              if (subscriptionState is OneSignalSubscriptionFailure) {
-                return _AlertBanner(
-                  title: subscriptionState.title,
-                  message: Text(
-                    subscriptionState.message,
-                  ),
-                  buttonOne: TextButton(
-                    child: Text('LEARN MORE'),
-                    onPressed: () async {
-                      launch(
-                        'https://github.com/Tautulli/Tautulli-Remote/wiki/OneSignal#registering',
+    return BlocBuilder<SettingsBloc, SettingsState>(
+      builder: (context, state) {
+        if (state is SettingsLoadSuccess) {
+          return BlocBuilder<OneSignalHealthBloc, OneSignalHealthState>(
+            builder: (context, healthState) {
+              return BlocBuilder<OneSignalSubscriptionBloc,
+                  OneSignalSubscriptionState>(
+                builder: (context, subscriptionState) {
+                  // Display alert banner about consenting to privacy policy
+                  if (!state.oneSignalBannerDismissed &&
+                      oneSignalPrivacyBloc.state
+                          is OneSignalPrivacyConsentFailure) {
+                    if (subscriptionState is OneSignalSubscriptionFailure) {
+                      return _AlertBanner(
+                        title: subscriptionState.title,
+                        message: Text(
+                          subscriptionState.message,
+                        ),
+                        buttonOne: TextButton(
+                          child: Text('DISMISS'),
+                          onPressed: () => settingsBloc.add(
+                            SettingsUpdateOneSignalBannerDismiss(true),
+                          ),
+                        ),
+                        buttonTwo: TextButton(
+                          child: Text('VIEW PRIVACY PAGE'),
+                          onPressed: () =>
+                              Navigator.of(context).pushNamed('/privacy'),
+                        ),
+                        backgroundColor: Colors.deepOrange[900],
                       );
-                    },
-                  ),
-                  backgroundColor: Colors.deepOrange[800],
-                );
-              }
-            }
-            // Return no banner if above criteria are not met
-            return Container(height: 0, width: 0);
-          },
-        );
+                    }
+                  }
+                  // Display alert banner about failure to connect to onesignal.com
+                  if (oneSignalPrivacyBloc.state
+                          is OneSignalPrivacyConsentSuccess &&
+                      healthState is OneSignalHealthFailure) {
+                    return _AlertBanner(
+                      title: 'Unable to reach OneSignal',
+                      message: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('• Notifications will not work.'),
+                          Text('• Registration with OneSignal will fail.'),
+                        ],
+                      ),
+                      buttonOne: TextButton(
+                        child: Text('CHECK AGAIN'),
+                        onPressed: () => context
+                            .read<OneSignalHealthBloc>()
+                            .add(OneSignalHealthCheck()),
+                      ),
+                    );
+                  }
+                  // Display alert banner about waiting to subscribe to OneSignal
+                  if (oneSignalPrivacyBloc.state
+                      is OneSignalPrivacyConsentSuccess) {
+                    if (subscriptionState is OneSignalSubscriptionFailure) {
+                      return _AlertBanner(
+                        title: subscriptionState.title,
+                        message: Text(
+                          subscriptionState.message,
+                        ),
+                        buttonOne: TextButton(
+                          child: Text('LEARN MORE'),
+                          onPressed: () async {
+                            launch(
+                              'https://github.com/Tautulli/Tautulli-Remote/wiki/OneSignal#registering',
+                            );
+                          },
+                        ),
+                        backgroundColor: Colors.deepOrange[800],
+                      );
+                    }
+                  }
+                  // Return no banner if above criteria are not met
+                  return Container(height: 0, width: 0);
+                },
+              );
+            },
+          );
+        }
+        return SizedBox(height: 0, width: 0);
       },
     );
   }
