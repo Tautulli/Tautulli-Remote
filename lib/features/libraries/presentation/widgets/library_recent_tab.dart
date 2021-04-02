21a4a7d665863c284e043af134527e8830ef7c09
TheMeanCanEHdian <drivard100@gmail.com>
2021-04-01 21:07:18 -0400

    Add library recent tab.

---
+++ lib/features/libraries/presentation/widgets/library_recent_tab.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/database/domain/entities/server.dart';
+import '../../../../core/widgets/bottom_loader.dart';
+import '../../../../core/widgets/poster_card.dart';
+import '../../../../injection_container.dart' as di;
+import '../../../media/domain/entities/media_item.dart';
+import '../../../media/presentation/pages/media_item_page.dart';
+import '../../../recent/presentation/bloc/libraries_recently_added_bloc.dart';
+import '../../../recent/presentation/widgets/recently_added_details.dart';
+import '../../../settings/presentation/bloc/settings_bloc.dart';
+
+class LibraryRecentTab extends StatelessWidget {
+  final int sectionId;
+
+  const LibraryRecentTab({
+    Key key,
+    @required this.sectionId,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => di.sl<LibrariesRecentlyAddedBloc>(),
+      child: _LibraryRecentTabContent(
+        sectionId: sectionId,
+      ),
+    );
+  }
+}
+
+class _LibraryRecentTabContent extends StatefulWidget {
+  final int sectionId;
+
+  _LibraryRecentTabContent({
+    Key key,
+    @required this.sectionId,
+  }) : super(key: key);
+
+  @override
+  __LibraryRecentTabContentState createState() =>
+      __LibraryRecentTabContentState();
+}
+
+class __LibraryRecentTabContentState extends State<_LibraryRecentTabContent> {
+  final _scrollController = ScrollController();
+  final _scrollThreshold = 200.0;
+  SettingsBloc _settingsBloc;
+  LibrariesRecentlyAddedBloc _librariesRecentlyAddedBloc;
+  String _tautulliId;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+    _settingsBloc = context.read<SettingsBloc>();
+    _librariesRecentlyAddedBloc = context.read<LibrariesRecentlyAddedBloc>();
+
+    final settingsState = _settingsBloc.state;
+
+    if (settingsState is SettingsLoadSuccess) {
+      String lastSelectedServer;
+
+      if (settingsState.lastSelectedServer != null) {
+        for (Server server in settingsState.serverList) {
+          if (server.tautulliId == settingsState.lastSelectedServer) {
+            lastSelectedServer = settingsState.lastSelectedServer;
+            break;
+          }
+        }
+      }
+
+      if (lastSelectedServer != null) {
+        setState(() {
+          _tautulliId = lastSelectedServer;
+        });
+      } else if (settingsState.serverList.length > 0) {
+        setState(() {
+          _tautulliId = settingsState.serverList[0].tautulliId;
+        });
+      } else {
+        _tautulliId = null;
+      }
+
+      _librariesRecentlyAddedBloc.add(
+        LibrariesRecentlyAddedFetch(
+          tautulliId: _tautulliId,
+          sectionId: widget.sectionId,
+          settingsBloc: _settingsBloc,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LibrariesRecentlyAddedBloc, LibrariesRecentlyAddedState>(
+      builder: (context, state) {
+        if (state is LibrariesRecentlyAddedSuccess) {
+          return MediaQuery.removePadding(
+            context: context,
+            removeTop: true,
+            child: Scrollbar(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  final heroTag = UniqueKey();
+
+                  return index >= state.list.length
+                      ? BottomLoader()
+                      : GestureDetector(
+                          onTap: () {
+                            final recentItem = state.list[index];
+
+                            MediaItem mediaItem = MediaItem(
+                              grandparentTitle: recentItem.grandparentTitle,
+                              parentMediaIndex: recentItem.parentMediaIndex,
+                              mediaIndex: recentItem.mediaIndex,
+                              mediaType: recentItem.mediaType,
+                              parentTitle: recentItem.parentTitle,
+                              posterUrl: recentItem.posterUrl,
+                              ratingKey: recentItem.ratingKey,
+                              title: recentItem.title,
+                              year: recentItem.year,
+                            );
+
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => MediaItemPage(
+                                  item: mediaItem,
+                                  heroTag: heroTag,
+                                  enableNavOptions: true,
+                                ),
+                              ),
+                            );
+                          },
+                          child: PosterCard(
+                            heroTag: heroTag,
+                            item: state.list[index],
+                            details: RecentlyAddedDetails(
+                              recentItem: state.list[index],
+                            ),
+                          ),
+                        );
+                },
+                itemCount: state.hasReachedMax
+                    ? state.list.length
+                    : state.list.length + 1,
+                controller: _scrollController,
+              ),
+            ),
+          );
+        }
+        if (state is LibrariesRecentlyAddedFailure) {
+          return Text('FAILURE');
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (maxScroll - currentScroll <= _scrollThreshold) {
+      _librariesRecentlyAddedBloc.add(
+        LibrariesRecentlyAddedFetch(
+          tautulliId: _tautulliId,
+          sectionId: widget.sectionId,
+          settingsBloc: _settingsBloc,
+        ),
+      );
+    }
+  }
+}
