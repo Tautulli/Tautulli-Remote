fb3c47a5db1d93bbfe673f2648133ed41ebc755d
TheMeanCanEHdian <drivard100@gmail.com>
2021-04-02 15:32:57 -0400

    Update build and changelog.

--- lib/features/changelog/data/datasource/changelog_data_source.dart
+++ lib/features/changelog/data/datasource/changelog_data_source.dart
@@ -11,6 +11,11 @@ Map changelog = {
         },
         {
           "type": "improvement",
+          "detail":
+              "Navigating back from other main pages will go to the activity page instead of trying to leave the app",
+        },
+        {
+          "type": "improvement",
           "detail": "Added transcode decision filter to history page",
         },
         {
