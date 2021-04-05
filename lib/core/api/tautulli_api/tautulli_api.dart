e927f326c924c8c05b1ae9f4e69c17b6b256d0f2
TheMeanCanEHdian <drivard100@gmail.com>
2021-04-05 18:04:46 -0400

    Add API call for get_plays_by_date.

--- lib/core/api/tautulli_api/tautulli_api.dart
+++ lib/core/api/tautulli_api/tautulli_api.dart
@@ -12,6 +12,7 @@ export 'get_library_media_info.dart';
 export 'get_library_user_stats.dart';
 export 'get_library_watch_time_stats.dart';
 export 'get_metadata.dart';
+export 'get_plays_by_date.dart';
 export 'get_recently_added.dart';
 export 'get_server_info.dart';
 export 'get_settings.dart';
