6e6fc032a19fc5886cb3cd618b908fa0117cb2e5
TheMeanCanEHdian <drivard100@gmail.com>
2021-04-08 13:28:38 -0400

    Add server selector and graph headings.

--- lib/features/graphs/presentation/pages/graphs_page.dart
+++ lib/features/graphs/presentation/pages/graphs_page.dart
@@ -5,9 +5,11 @@ import '../../../../core/database/domain/entities/server.dart';
 import '../../../../core/widgets/app_drawer.dart';
 import '../../../../core/widgets/app_drawer_icon.dart';
 import '../../../../core/widgets/double_tap_exit.dart';
+import '../../../../core/widgets/server_header.dart';
 import '../../../../injection_container.dart' as di;
 import '../../../settings/presentation/bloc/settings_bloc.dart';
 import '../bloc/graphs_bloc.dart';
+import '../widgets/graph_heading.dart';
 import '../widgets/plays_by_date_graph.dart';
 
 class GraphsPage extends StatelessWidget {
@@ -82,24 +84,78 @@ class __GraphsPageContentState extends State<_GraphsPageContent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
         title: const Text('Graphs'),
         leading: const AppDrawerIcon(),
       ),
       drawer: const AppDrawer(),
       body: DoubleTapExit(
-        child: BlocBuilder<GraphsBloc, GraphsState>(
-          builder: (context, state) {
-            if (state is GraphsSuccess) {
-              return PlaysByDateGraph(playsByDate: state.playsByDate);
-            }
-            if (state is GraphsFailure) {
-              return const Text('FAILURE');
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            BlocBuilder<SettingsBloc, SettingsState>(
+              builder: (context, state) {
+                if (state is SettingsLoadSuccess) {
+                  if (state.serverList.length > 1) {
+                    return DropdownButtonHideUnderline(
+                      child: DropdownButton(
+                        value: _tautulliId,
+                        style: TextStyle(color: Theme.of(context).accentColor),
+                        items: state.serverList.map((server) {
+                          return DropdownMenuItem(
+                            child: ServerHeader(serverName: server.plexName),
+                            value: server.tautulliId,
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          if (value != _tautulliId) {
+                            setState(() {
+                              _tautulliId = value;
+                            });
+                            _settingsBloc.add(
+                              SettingsUpdateLastSelectedServer(
+                                  tautulliId: _tautulliId),
+                            );
+                            _graphsBloc.add(
+                              GraphsFilter(
+                                tautulliId: value,
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                    );
+                  }
+                }
+                return Container(height: 0, width: 0);
+              },
+            ),
+            BlocBuilder<GraphsBloc, GraphsState>(
+              builder: (context, state) {
+                if (state is GraphsSuccess) {
+                  return Expanded(
+                    child: Scrollbar(
+                      child: ListView(
+                        children: [
+                          const GraphHeading(graphHeading: 'Daily Play Count'),
+                          PlaysByDateGraph(playsByDate: state.playsByDate),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                if (state is GraphsFailure) {
+                  return const Text('FAILURE');
+                }
+                return const Expanded(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
