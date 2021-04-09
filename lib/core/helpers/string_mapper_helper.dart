772e909575b368865a0d326e9a02fe17d1b8745d
TheMeanCanEHdian <drivard100@gmail.com>
2021-04-09 17:05:05 -0400

    Add title mapper for graph series type.

--- lib/core/helpers/string_mapper_helper.dart
+++ lib/core/helpers/string_mapper_helper.dart
@@ -1,3 +1,5 @@
+import '../../features/graphs/domain/entities/series_data.dart';
+
 class StringMapperHelper {
   static String mapStatIdToTitle(String statId) {
     switch (statId) {
@@ -23,6 +25,21 @@ class StringMapperHelper {
         return 'Most Concurrent Streams';
       case ('top_libraries'):
         return 'Most Active Libraries';
+      default:
+        return 'UNKNOWN';
+    }
+  }
+
+  static String mapSeriesTypeToTitle(SeriesType seriesType) {
+    switch (seriesType) {
+      case (SeriesType.tv):
+        return 'TV';
+      case (SeriesType.movies):
+        return 'Movies';
+      case (SeriesType.music):
+        return 'Music';
+      case (SeriesType.live):
+        return 'Live TV';
       default:
         return 'UNKNOWN';
     }
