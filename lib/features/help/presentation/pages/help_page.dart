ae04372f5bcf9ad0a5a086ecfb31ababeddcb7c2
TheMeanCanEHdian <drivard100@gmail.com>
2021-06-09 10:08:11 -0500

    Fix imports.

--- lib/features/help/presentation/pages/help_page.dart
+++ lib/features/help/presentation/pages/help_page.dart
@@ -1,11 +1,11 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:tautulli_remote/translations/locale_keys.g.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../core/helpers/color_palette_helper.dart';
 import '../../../../core/widgets/list_header.dart';
-import 'package:easy_localization/easy_localization.dart';
+import '../../../../translations/locale_keys.g.dart';
 
 class HelpPage extends StatelessWidget {
   const HelpPage({Key key}) : super(key: key);
