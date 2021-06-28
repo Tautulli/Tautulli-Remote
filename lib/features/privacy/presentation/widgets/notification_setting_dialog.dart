96e92513de2700f3605b8a27d4a810704dcbc32b
TheMeanCanEHdian <drivard100@gmail.com>
2021-06-28 18:57:00 -0500

    Update Privacy localization keys.

--- lib/features/privacy/presentation/widgets/notification_setting_dialog.dart
+++ lib/features/privacy/presentation/widgets/notification_setting_dialog.dart
@@ -1,21 +1,22 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+
+import '../../../../translations/locale_keys.g.dart';
 
 Future<void> showNotificationSettingsDialog(BuildContext context) {
   return showDialog(
     context: context,
     builder: (context) {
       return AlertDialog(
-        title: const Text('Notification Permission Required'),
-        content: const Text(
-          'Give Tautulli Remote notification access to receive push notitifications.',
-        ),
+        title: const Text(LocaleKeys.privacy_alert_title).tr(),
+        content: const Text(LocaleKeys.privacy_alert_content).tr(),
         actions: [
           TextButton(
             onPressed: () {
               Navigator.of(context).pop();
             },
-            child: const Text('CANCEL'),
+            child: const Text(LocaleKeys.button_cancel).tr(),
           ),
           TextButton(
             onPressed: () async {
@@ -27,7 +28,7 @@ Future<void> showNotificationSettingsDialog(BuildContext context) {
                 },
               );
             },
-            child: const Text('GO TO SETTINGS'),
+            child: const Text(LocaleKeys.button_go_to_settings).tr(),
           ),
         ],
       );
