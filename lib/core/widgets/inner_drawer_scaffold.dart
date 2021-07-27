8db3e28c8b0f113a15bfba9a445e5a2dc5d59a93
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-26 19:21:06 -0500

    Create InnerDrawerScaffold to pass page body into.

---
+++ lib/core/widgets/inner_drawer_scaffold.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inner_drawer/inner_drawer.dart';
+
+import 'app_drawer.dart';
+import 'app_drawer_icon.dart';
+import 'double_tap_exit.dart';
+
+class InnerDrawerScaffold extends StatelessWidget {
+  final Widget title;
+  final Widget body;
+  final List<Widget> actions;
+
+  const InnerDrawerScaffold({
+    Key key,
+    @required this.title,
+    @required this.body,
+    this.actions,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<InnerDrawerState> _innerDrawerKey =
+        GlobalKey<InnerDrawerState>();
+
+    return InnerDrawer(
+      key: _innerDrawerKey,
+      onTapClose: true,
+      offset: const IDOffset.horizontal(0.6),
+      leftChild: const AppDrawer(),
+      scaffold: Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        appBar: AppBar(
+          leading: AppDrawerIcon(innerDrawerKey: _innerDrawerKey),
+          title: title,
+          actions: actions ?? [],
+        ),
+        body: DoubleTapExit(
+          child: body,
+        ),
+      ),
+    );
+  }
+}
