3e6769999d3e7494708b45e8bed9b5b789ead0e0
TheMeanCanEHdian <drivard100@gmail.com>
2021-08-20 12:51:53 -0500

    Add donate button to Changelog.

--- lib/features/changelog/presentation/pages/changelog_page.dart
+++ lib/features/changelog/presentation/pages/changelog_page.dart
@@ -1,7 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../translations/locale_keys.g.dart';
+import '../../../donate/presentation/pages/donate_page.dart';
 import '../../data/datasource/changelog_data_source.dart';
 import '../widgets/changelog_item.dart';
 
@@ -16,6 +18,21 @@ class ChangelogPage extends StatelessWidget {
       backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
         title: const Text(LocaleKeys.changelog_page_title).tr(),
+        actions: [
+          TextButton.icon(
+            onPressed: () {
+              Navigator.of(context).pushNamed(DonatePage.routeName);
+            },
+            icon: const FaIcon(
+              FontAwesomeIcons.solidHeart,
+              color: Colors.red,
+              size: 18,
+            ),
+            label: Text(
+              LocaleKeys.donate_page_title.tr(),
+            ),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
