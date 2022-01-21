7333f26a4028d88ba4c289fcd269b058abf600f9
TheMeanCanEHdian <drivard100@gmail.com>
2022-01-21 13:23:24 -0600

    Add dependency injection framework.

---
+++ lib/dependency_injection.dart
@@ -0,0 +1,6 @@
+// Service locator alias
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'tautulli_remote.dart';
+import 'dependency_injection.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
 
   runApp(
     const TautulliRemote(),
