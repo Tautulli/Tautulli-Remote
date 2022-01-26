ef26b8c2b71f1c4ff9f51a64c13af63edddab614
TheMeanCanEHdian <drivard100@gmail.com>
2022-01-26 15:25:27 -0600

    Add OneSignalDataSource.

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,9 +1,13 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/local_storage/local_storage.dart';
 import 'core/manage_cache/manage_cache.dart';
+import 'core/network_info/network_info.dart';
+import 'features/onesignal/data/datasources/onesignal_data_source.dart';
 import 'features/settings/data/datasources/settings_data_source.dart';
 import 'features/settings/data/repositories/settings_repository_impl.dart';
 import 'features/settings/domain/repositories/settings_repository.dart';
@@ -14,18 +18,33 @@ import 'features/settings/presentation/bloc/settings_bloc.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  //! Core - Local Storage
+  //! Core
   sl.registerLazySingleton<LocalStorage>(
     () => LocalStorageImpl(sl()),
   );
   sl.registerLazySingleton<ManageCache>(
     () => ManageCacheImpl(sl()),
   );
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(sl()),
+  );
 
   //! External
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton(() => sharedPreferences);
   sl.registerLazySingleton(() => DefaultCacheManager());
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => Connectivity());
+
+  //! Features - OneSignal
+  // Data sources
+  sl.registerLazySingleton<OneSignalDataSource>(
+    () => OneSignalDataSourceImpl(
+      client: sl(),
+      networkInfo: sl(),
+      settings: sl(),
+    ),
+  );
 
   //! Features - Settings
   // Bloc
---
+++ lib/features/onesignal/data/datasources/onesignal_data_source.dart
@@ -0,0 +1,91 @@
+import 'package:http/http.dart' as http;
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+
+import '../../../../core/network_info/network_info.dart';
+import '../../../settings/domain/usecases/settings.dart';
+
+abstract class OneSignalDataSource {
+  /// Disables or enables push notifications
+  Future<void> disablePush(bool value);
+
+  /// Grants or revokes consent based on the provided boolean.
+  Future<void> grantConsent(bool value);
+
+  /// Returns `true` if the user has granted consent to the OneSignal SDK.
+  Future<bool> get hasConsented;
+
+  /// Checks if `https://onesignal.com` is reachable.
+  Future<bool> get isReachable;
+
+  /// Indicates if the user is subscribed to OneSignal.
+  Future<bool> get isSubscribed;
+
+  /// Provides the OneSignal User ID (AKA playerID).
+  ///
+  /// Returns `'onesignal-disabled'` if an error is thrown.
+  Future<String> get userId;
+}
+
+class OneSignalDataSourceImpl implements OneSignalDataSource {
+  final http.Client client;
+  final NetworkInfo networkInfo;
+  final Settings settings;
+
+  OneSignalDataSourceImpl({
+    required this.client,
+    required this.networkInfo,
+    required this.settings,
+  });
+
+  @override
+  Future<void> disablePush(bool value) async {
+    await OneSignal.shared.disablePush(value);
+  }
+
+  @override
+  Future<void> grantConsent(bool value) async {
+    await OneSignal.shared.consentGranted(value);
+  }
+
+  @override
+  Future<bool> get hasConsented async {
+    return await settings.getOneSignalConsented();
+  }
+
+  @override
+  Future<bool> get isReachable async {
+    if (await networkInfo.isConnected) {
+      final response = await client.head(Uri.parse('https://onesignal.com'));
+      if (response.statusCode == 200) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @override
+  Future<bool> get isSubscribed async {
+    final state = await OneSignal.shared.getDeviceState();
+    if (state != null) {
+      return state.subscribed;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  Future<String> get userId async {
+    try {
+      final OSDeviceState? status = await OneSignal.shared.getDeviceState();
+      if (status != null) {
+        final String? userId = status.userId;
+        if (userId != null) {
+          return userId;
+        }
+      }
+      return 'onesignal-disabled';
+    } catch (_) {
+      return 'onesignal-disabled';
+    }
+  }
+}
