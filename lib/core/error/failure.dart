8dcc115c158e03c2beb17bde978d7f7c051d3753
TheMeanCanEHdian <drivard100@gmail.com>
2022-02-04 20:45:50 -0600

    Update Exceptions and Failures.
    Add logic to map DioError responses to more specific Exceptions.
    
    Comment out currently unused Exceptions and Failures.

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,13 +1,13 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
-  final dynamic data;
-
-  const Failure({this.data});
-
   @override
-  List<Object> get props => [data];
+  List<Object> get props => [];
 }
+
+/// A 401 response containing 'Authorization Required' was returned. Typically,
+/// from missing basic authentication headers.
+class AuthorizationRequiredFailure extends Failure {}
 
 /// The TSL/SSL certificate is expired.
 class CertificateExpiredFailure extends Failure {}
@@ -15,31 +15,29 @@ class CertificateExpiredFailure extends Failure {}
 /// A failure in verifying the TSL/SSL certificate.
 class CertificateVerificationFailure extends Failure {}
 
-/// Connection details are missing.
-class ConnectionDetailsFailure extends Failure {}
+// /// Connection details are missing.
+// class ConnectionDetailsFailure extends Failure {}
 
 /// Device is not connected to a network.
 class ConnectionFailure extends Failure {}
 
-/// Error initalizing the database.
-class DatabaseInitFailure extends Failure {}
+// /// Error initalizing the database.
+// class DatabaseInitFailure extends Failure {}
 
 /// Error thrown by the dio HTTP client.
-class DioFailure extends Failure {
-  const DioFailure({dynamic data}) : super(data: data);
-
-  @override
-  List<Object> get props => [data];
-}
+class DioFailure extends Failure {}
 
 /// A catch-all Failure.
 class GenericFailure extends Failure {}
 
-/// Error parsing JSON.
-class JsonDecodeFailure extends Failure {}
+/// Tautulli has responded with the error 'Invalid apikey'.
+class InvalidApiKeyFailure extends Failure {}
 
-/// No servers are configured.
-class MissingServerFailure extends Failure {}
+// /// Error parsing JSON.
+// class JsonDecodeFailure extends Failure {}
+
+// /// No servers are configured.
+// class MissingServerFailure extends Failure {}
 
 // Server has provided an undesired response.
 class ServerFailure extends Failure {}
@@ -47,17 +45,17 @@ class ServerFailure extends Failure {}
 /// Server min version is not met.
 class ServerVersionFailure extends Failure {}
 
-/// Required settings are missing.
-class SettingsFailure extends Failure {}
+// /// Required settings are missing.
+// class SettingsFailure extends Failure {}
 
-/// Unable to connect to a provided address.
-class SocketFailure extends Failure {}
+// /// Unable to connect to a provided address.
+// class SocketFailure extends Failure {}
 
-/// Time to connect to a server exceeded.
-class TimeoutFailure extends Failure {}
+// /// Time to connect to a server exceeded.
+// class TimeoutFailure extends Failure {}
 
-/// A failure in TLS/SSL connection.
-class TlsFailure extends Failure {}
+// /// A failure in TLS/SSL connection.
+// class TlsFailure extends Failure {}
 
-/// URL provided is improperly formatted.
-class UrlFormatFailure extends Failure {}
+// /// URL provided is improperly formatted.
+// class UrlFormatFailure extends Failure {}
--- lib/core/helpers/failure_helper.dart
+++ lib/core/helpers/failure_helper.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:quiver/strings.dart';
 
 import '../../dependency_injection.dart' as di;
 import '../../features/logging/domain/usecases/logging.dart';
@@ -14,19 +14,22 @@ const String certificateExpiredMessage = 'TLS/SSL Certificate is Expired.';
 const String certificateVerificationMessage =
     'Certificate verification failed.';
 const String connectionMessage = 'No network connectivity.';
-const String dataBaseInitMessage = 'Failed to initalize database.';
+// const String dataBaseInitMessage = 'Failed to initalize database.';
 const String genericMessage = 'Unknown error.';
-const String jsonMessage = 'Failed to parse response.';
-const String missingServerMessage = 'No servers are configured.';
+const String invalidApiKeyMessage = 'Invalid Device Token';
+// const String jsonMessage = 'Failed to parse response.';
+// const String missingServerMessage = 'No servers are configured.';
 const String serverMessage = 'Failed to connect to server.';
 const String serverVersionMessage =
     'Server version does not meet requirements.';
-const String settingsMessage = 'Required settings are missing.';
-const String socketMessage = 'Failed to connect to Connection Address.';
-const String timeoutMessage = 'Connection to server timed out.';
-const String tlsMessage = 'Failed to establish TLS/SSL connection.';
+// const String settingsMessage = 'Required settings are missing.';
+// const String socketMessage = 'Failed to connect to Connection Address.';
+// const String timeoutMessage = 'Connection to server timed out.';
+// const String tlsMessage = 'Failed to establish TLS/SSL connection.';
 
 //* Error Suggestions
+const String authorizationRequiredSuggestion =
+    'Server responded with Authorization Required. Verify connection details and headers.';
 const String certificateExpiredSuggestion =
     'Please check your certificate and re-register with your Tautulli server.';
 const String certificateVerificationSuggestion =
@@ -36,8 +39,10 @@ const String checkConnectionAddressSuggestion =
 const String checkServerSettingsSuggestion =
     'Please verify your connection settings.';
 const String genericSuggestion = 'Please contact Support.';
-const String missingServerSuggestion =
-    'Please register with a Tautulli server.';
+// const String missingServerSuggestion =
+//     'Please register with a Tautulli server.';
+const String invalidApiKeySuggestion =
+    'Check your device token or try generating a new QR code on Tautulli.';
 const String plexConnectionSuggestion =
     'Check your Connection Address for errors and make sure Tautulli can communicate with Plex.';
 final String serverVersionSuggestion =
@@ -48,14 +53,12 @@ class FailureHelper {
   ///
   /// Unknown exceptions will map to `GenericFailure`.
   static Failure castToFailure(dynamic exception) {
-    Response? dioResponse;
-
     if ([
       TimeoutException,
-      SettingsException,
-      JsonDecodeException,
-      HandshakeException,
-      SocketException,
+      // SettingsException,
+      // JsonDecodeException,
+      // HandshakeException,
+      // SocketException,
       ServerException,
       ServerVersionException,
       // DioError,
@@ -63,42 +66,60 @@ class FailureHelper {
       exception = exception.runtimeType;
     }
 
+    // Parse DioError responses to map to more specific errors
     if (exception.runtimeType == DioError) {
-      dioResponse = exception.response;
-      exception = exception.runtimeType;
+      print('EXCEPTION RESPONSE: ${exception.response}');
+
+      final responseString = exception.response.toString();
+
+      if (responseString.toLowerCase().contains('authorization required')) {
+        exception = AuthorizationRequiredException;
+      } else if (responseString.contains('"message":"Invalid apikey"')) {
+        exception = InvalidApiKeyException;
+      } else {
+        if (isNotBlank(responseString)) {
+          di.sl<Logging>().error(
+                'FailureMapper :: Unaccounted for HTTP client error response [$responseString]',
+              );
+        }
+        exception = ServerException;
+      }
     }
 
     switch (exception) {
+      case (AuthorizationRequiredException):
+        return AuthorizationRequiredFailure();
       case (CertificateExpiredException):
         return CertificateExpiredFailure();
       case (CertificateVerificationException):
         return CertificateVerificationFailure();
-      case (ConnectionDetailsException):
-        return ConnectionDetailsFailure();
-      case (DatabaseInitException):
-        return DatabaseInitFailure();
+      // case (ConnectionDetailsException):
+      //   return ConnectionDetailsFailure();
+      // case (DatabaseInitException):
+      //   return DatabaseInitFailure();
       case (DioError):
-        //TODO: More granular failures, could also provide tautulli error message
-        return DioFailure(data: dioResponse);
-      case (HandshakeException):
-        return TlsFailure();
-      case (JsonDecodeException):
-        return JsonDecodeFailure();
+        return DioFailure();
+      case (InvalidApiKeyException):
+        return InvalidApiKeyFailure();
+      // case (HandshakeException):
+      //   return TlsFailure();
+      // case (JsonDecodeException):
+      //   return JsonDecodeFailure();
       case (ServerException):
         return ServerFailure();
       case (ServerVersionException):
         return ServerVersionFailure();
-      case (SettingsException):
-        return SettingsFailure();
-      case (SocketException):
-        return SocketFailure();
-      case (TimeoutException):
-        return TimeoutFailure();
-      case (TlsException):
-        return TlsFailure();
+      // case (SettingsException):
+      //   return SettingsFailure();
+      // case (SocketException):
+      //   return SocketFailure();
+      // case (TimeoutException):
+      //   return TimeoutFailure();
+      // case (TlsException):
+      //   return TlsFailure();
       default:
         di.sl<Logging>().error(
-              'Unable to map [$exception] to a specific failure',
+              'FailureMapper :: Unable to map [$exception] to a specific failure',
             );
         return GenericFailure();
     }
@@ -106,32 +127,37 @@ class FailureHelper {
 
   static String mapFailureToMessage(Failure failure) {
     switch (failure.runtimeType) {
+      case (AuthorizationRequiredFailure):
+        return serverMessage;
       case (CertificateExpiredFailure):
         return certificateExpiredMessage;
       case (CertificateVerificationFailure):
         return certificateVerificationMessage;
-      case (ConnectionDetailsFailure):
-        return settingsMessage;
+      // case (ConnectionDetailsFailure):
+      //   return settingsMessage;
       case (ConnectionFailure):
         return connectionMessage;
-      case (DatabaseInitFailure):
-        return dataBaseInitMessage;
-      case (JsonDecodeFailure):
-        return jsonMessage;
-      case (MissingServerFailure):
-        return missingServerMessage;
+      // case (DatabaseInitFailure):
+      //   return dataBaseInitMessage;
+      case (InvalidApiKeyFailure):
+        return invalidApiKeyMessage;
+      // case (JsonDecodeFailure):
+      //   return jsonMessage;
+      // case (MissingServerFailure):
+      //   return missingServerMessage;
+      case (DioFailure):
       case (ServerFailure):
         return serverMessage;
       case (ServerVersionFailure):
         return serverVersionMessage;
-      case (SettingsFailure):
-        return settingsMessage;
-      case (SocketFailure):
-        return socketMessage;
-      case (TimeoutFailure):
-        return timeoutMessage;
-      case (TlsFailure):
-        return tlsMessage;
+      // case (SettingsFailure):
+      //   return settingsMessage;
+      // case (SocketFailure):
+      //   return socketMessage;
+      // case (TimeoutFailure):
+      //   return timeoutMessage;
+      // case (TlsFailure):
+      //   return tlsMessage;
       case (GenericFailure):
       default:
         return genericMessage;
@@ -140,32 +166,37 @@ class FailureHelper {
 
   static String mapFailureToSuggestion(Failure failure) {
     switch (failure.runtimeType) {
+      case (AuthorizationRequiredFailure):
+        return authorizationRequiredSuggestion;
       case (CertificateExpiredFailure):
         return certificateExpiredSuggestion;
       case (CertificateVerificationFailure):
         return certificateVerificationSuggestion;
-      case (ConnectionDetailsFailure):
-        return checkServerSettingsSuggestion;
+      // case (ConnectionDetailsFailure):
+      //   return checkServerSettingsSuggestion;
       case (ConnectionFailure):
         return '';
-      case (DatabaseInitFailure):
-        return genericSuggestion;
-      case (JsonDecodeFailure):
-        return checkConnectionAddressSuggestion;
-      case (MissingServerFailure):
-        return missingServerSuggestion;
+      // case (DatabaseInitFailure):
+      //   return genericSuggestion;
+      case (InvalidApiKeyFailure):
+        return invalidApiKeySuggestion;
+      // case (JsonDecodeFailure):
+      //   return checkConnectionAddressSuggestion;
+      // case (MissingServerFailure):
+      //   return missingServerSuggestion;
+      case (DioFailure):
       case (ServerFailure):
         return checkServerSettingsSuggestion;
       case (ServerVersionFailure):
         return serverVersionSuggestion;
-      case (SettingsFailure):
-        return checkServerSettingsSuggestion;
-      case (SocketFailure):
-        return checkConnectionAddressSuggestion;
-      case (TimeoutFailure):
-        return plexConnectionSuggestion;
-      case (TlsFailure):
-        return checkConnectionAddressSuggestion;
+      // case (SettingsFailure):
+      //   return checkServerSettingsSuggestion;
+      // case (SocketFailure):
+      //   return checkConnectionAddressSuggestion;
+      // case (TimeoutFailure):
+      //   return plexConnectionSuggestion;
+      // case (TlsFailure):
+      //   return checkConnectionAddressSuggestion;
       case (GenericFailure):
       default:
         return genericSuggestion;
