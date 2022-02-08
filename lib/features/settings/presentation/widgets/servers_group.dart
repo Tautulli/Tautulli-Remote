886fc9de89b807a2eae2bda4e2cd3daade3c4773
TheMeanCanEHdian <drivard100@gmail.com>
2022-02-08 10:58:56 -0600

    Build ListTiles from loaded servers.

--- lib/features/settings/presentation/widgets/servers_group.dart
+++ lib/features/settings/presentation/widgets/servers_group.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
 import '../../../../core/widgets/list_tile_group.dart';
+import '../bloc/settings_bloc.dart';
 import '../pages/server_settings_page.dart';
 import 'settings_list_tile.dart';
 
@@ -10,25 +12,35 @@ class ServersGroup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTileGroup(
-      heading: 'Servers',
-      listTiles: [
-        SettingsListTile(
-          sensitive: true,
-          leading: WebsafeSvg.asset(
-            'assets/logos/logo_flat.svg',
-            color: Theme.of(context).colorScheme.tertiary,
-            height: 35,
-          ),
-          title: 'Server Name',
-          subtitle: 'https://tautulli.domain.com',
-          onTap: () => Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => const ServerSettingsPage(),
-            ),
-          ),
-        ),
-      ],
+    return BlocBuilder<SettingsBloc, SettingsState>(
+      builder: (context, state) {
+        return ListTileGroup(
+          heading: 'Servers',
+          listTiles: state is SettingsSuccess
+              ? state.serverList
+                  .map(
+                    (server) => SettingsListTile(
+                      sensitive: true,
+                      leading: WebsafeSvg.asset(
+                        'assets/logos/logo_flat.svg',
+                        color: Theme.of(context).colorScheme.tertiary,
+                        height: 35,
+                      ),
+                      title: server.plexName,
+                      subtitle: server.primaryActive!
+                          ? server.primaryConnectionAddress
+                          : server.secondaryConnectionAddress,
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const ServerSettingsPage(),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList()
+              : [],
+        );
+      },
     );
   }
 }
