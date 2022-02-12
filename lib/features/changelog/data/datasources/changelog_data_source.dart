6c398579ff3854c840219a0f24dab9045af1ff6d
TheMeanCanEHdian <drivard100@gmail.com>
2022-02-12 09:11:46 -0600

    Update changelog.

--- lib/features/changelog/data/datasources/changelog_data_source.dart
+++ lib/features/changelog/data/datasources/changelog_data_source.dart
@@ -2,17 +2,31 @@ Map changelog = {
   "data": [
     {
       "version": "v3.0.0-alpha.1",
-      "date": "January 31, 2022",
+      "date": "Feb 13, 2022",
       "changes": [
         {
           "type": "important",
           "detail":
-              "Version 3 has been rewritten and redesigned from the ground up",
+              "Version 3 is a work in progress being re-written from the ground up",
+        },
+        {
+          "type": "important",
+          "detail":
+              "Check the 'How To Test' page on how to submit feedback or bugs",
         },
         {
           "type": "important",
           "detail":
               "Donations cannot be tested in the alpha, they will always fail to load",
+        },
+        {
+          "type": "new",
+          "detail": "Core settings and related functions/pages have been added",
+        },
+        {
+          "type": "new",
+          "detail":
+              "A new data dump page for testing had been added to view current app, server, and OneSignal information",
         },
       ],
     },
