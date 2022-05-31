334100a031876b85d39121097a7a457433408a0c
TheMeanCanEHdian <drivard100@gmail.com>
2022-05-30 20:51:04 -0500

    Use CachedNetworkImageProvider in UserDetails.

--- lib/features/users/presentation/pages/user_details_page.dart
+++ lib/features/users/presentation/pages/user_details_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -221,7 +222,7 @@ Future<Color?> _getColor(String? url) async {
   if (url == null || !url.startsWith('http')) return null;
 
   final palette = await PaletteGenerator.fromImageProvider(
-    NetworkImage(url),
+    CachedNetworkImageProvider(url),
     maximumColorCount: 1,
   );
 
