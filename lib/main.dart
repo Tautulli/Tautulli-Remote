09a8d4c47ab6904a191cc859a929f92ee60918d0
TheMeanCanEHdian <drivard100@gmail.com>
2022-05-31 07:23:18 -0500

    Make sure images load for manually trusted certs.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,10 +18,36 @@ import 'features/settings/presentation/bloc/settings_bloc.dart';
 import 'tautulli_remote.dart';
 import 'translations/codegen_loader.g.dart';
 
+/// Create an [HttpOverride] for [createHttpClient] to check cert failures
+/// against the saved cert hash list.
+class MyHttpOverrides extends HttpOverrides {
+  final List<int> customCertHashList;
+
+  MyHttpOverrides(this.customCertHashList);
+
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) {
+        int certHashCode = cert.pem.hashCode;
+
+        if (customCertHashList.contains(certHashCode)) {
+          return true;
+        }
+        return false;
+      };
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await di.init();
+
+  // Override global HttpClient to check for trusted cert hashes on certificate failure.
+  final List<int> customCertHashList =
+      await di.sl<Settings>().getCustomCertHashList();
+  HttpOverrides.global = MyHttpOverrides(customCertHashList);
 
   Future<String?> calculateInitialRoute() async {
     final runningVersion = await PackageInformationImpl().version;
