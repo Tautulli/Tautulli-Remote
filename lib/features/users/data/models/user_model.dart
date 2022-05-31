001434b3020b80329bd320e0ed04f7c3b82ab8d3
TheMeanCanEHdian <drivard100@gmail.com>
2022-05-30 19:10:40 -0500

    Update user models.

--- lib/features/users/data/models/user_model.dart
+++ lib/features/users/data/models/user_model.dart
@@ -1,7 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-import '../../../../core/types/types.dart';
 import '../../../../core/utilities/cast.dart';
 
 part 'user_model.g.dart';
@@ -12,95 +11,67 @@ class UserModel extends Equatable {
   final bool? allowGuest;
   @JsonKey(name: 'do_notify', fromJson: Cast.castToBool)
   final bool? doNotify;
-  @JsonKey(name: 'duration', fromJson: Cast.castToInt)
-  final int? duration;
   @JsonKey(name: 'email', fromJson: Cast.castToString)
   final String? email;
+  @JsonKey(name: 'filter_all', fromJson: Cast.castToString)
+  final String? filterAll;
+  @JsonKey(name: 'filter_movies', fromJson: Cast.castToString)
+  final String? filterMovies;
+  @JsonKey(name: 'filter_music', fromJson: Cast.castToString)
+  final String? filterMusic;
+  @JsonKey(name: 'filter_photos', fromJson: Cast.castToString)
+  final String? filterPhotos;
+  @JsonKey(name: 'filter_tv', fromJson: Cast.castToString)
+  final String? filterTv;
   @JsonKey(name: 'friendly_name', fromJson: Cast.castToString)
   final String? friendlyName;
-  @JsonKey(name: 'guid', fromJson: Cast.castToString)
-  final String? guid;
-  @JsonKey(name: 'history_row_id', fromJson: Cast.castToInt)
-  final int? historyRowId;
-  @JsonKey(name: 'ip_address', fromJson: Cast.castToString)
-  final String? ipAddress;
   @JsonKey(name: 'is_active', fromJson: Cast.castToBool)
   final bool? isActive;
+  @JsonKey(name: 'is_admin', fromJson: Cast.castToBool)
+  final bool? isAdmin;
+  @JsonKey(name: 'is_allow_sync', fromJson: Cast.castToBool)
+  final bool? isAllowSync;
+  @JsonKey(name: 'is_home_user', fromJson: Cast.castToBool)
+  final bool? isHomeUser;
+  @JsonKey(name: 'is_restricted', fromJson: Cast.castToBool)
+  final bool? isRestricted;
   @JsonKey(name: 'keep_history', fromJson: Cast.castToBool)
   final bool? keepHistory;
-  @JsonKey(name: 'last_played', fromJson: Cast.castToString)
-  final String? lastPlayed;
   @JsonKey(name: 'last_seen', fromJson: Cast.castToInt)
   final int? lastSeen;
-  @JsonKey(name: 'live', fromJson: Cast.castToBool)
-  final bool? live;
-  @JsonKey(name: 'media_index', fromJson: Cast.castToInt)
-  final int? mediaIndex;
-  @JsonKey(name: 'media_type', fromJson: Cast.castStringToMediaType)
-  final MediaType? mediaType;
-  @JsonKey(name: 'originally_available_at', fromJson: Cast.castToString)
-  final String? originallyAvailableAt;
-  @JsonKey(name: 'parent_media_index', fromJson: Cast.castToInt)
-  final int? parentMediaIndex;
-  @JsonKey(name: 'parent_title', fromJson: Cast.castToString)
-  final String? parentTitle;
-  @JsonKey(name: 'platform', fromJson: Cast.castToString)
-  final String? platform;
-  @JsonKey(name: 'player', fromJson: Cast.castToString)
-  final String? player;
-  @JsonKey(name: 'plays', fromJson: Cast.castToInt)
-  final int? plays;
-  @JsonKey(name: 'rating_key', fromJson: Cast.castToInt)
-  final int? ratingKey;
   @JsonKey(name: 'row_id', fromJson: Cast.castToInt)
   final int? rowId;
-  @JsonKey(name: 'thumb', fromJson: Cast.castToString)
-  final String? thumb;
-  @JsonKey(name: 'title', fromJson: Cast.castToString)
-  final String? title;
-  @JsonKey(
-      name: 'transcode_decision', fromJson: Cast.castStringToStreamDecision)
-  final StreamDecision? transcodeDecision;
-  @JsonKey(name: 'user_id', fromJson: Cast.castToInt)
-  final int? userId;
+  @JsonKey(name: 'shared_libraries', fromJson: sharedLibrariesFromJson)
+  final List<int>? sharedLibraries;
   @JsonKey(name: 'user_thumb', fromJson: Cast.castToString)
   final String? userThumb;
+  @JsonKey(name: 'user_id', fromJson: Cast.castToInt)
+  final int? userId;
   @JsonKey(name: 'username', fromJson: Cast.castToString)
   final String? username;
-  @JsonKey(name: 'year', fromJson: Cast.castToInt)
-  final int? year;
 
   const UserModel({
-    required this.allowGuest,
-    required this.doNotify,
-    required this.duration,
-    required this.friendlyName,
-    required this.username,
-    required this.title,
-    required this.email,
-    required this.guid,
-    required this.historyRowId,
-    required this.ipAddress,
-    required this.isActive,
-    required this.keepHistory,
-    required this.lastPlayed,
-    required this.lastSeen,
-    required this.live,
-    required this.mediaIndex,
-    required this.mediaType,
-    required this.originallyAvailableAt,
-    required this.parentMediaIndex,
-    required this.parentTitle,
-    required this.platform,
-    required this.player,
-    required this.plays,
-    required this.ratingKey,
-    required this.rowId,
-    required this.thumb,
-    required this.transcodeDecision,
-    required this.userId,
-    required this.userThumb,
-    required this.year,
+    this.allowGuest,
+    this.doNotify,
+    this.email,
+    this.filterAll,
+    this.filterMovies,
+    this.filterMusic,
+    this.filterPhotos,
+    this.filterTv,
+    this.friendlyName,
+    this.isActive,
+    this.isAdmin,
+    this.isAllowSync,
+    this.isHomeUser,
+    this.isRestricted,
+    this.keepHistory,
+    this.lastSeen,
+    this.rowId,
+    this.sharedLibraries,
+    this.userThumb,
+    this.userId,
+    this.username,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) =>
@@ -108,43 +79,34 @@ class UserModel extends Equatable {
 
   Map<String, dynamic> toJson() => _$UserModelToJson(this);
 
-  static List<int>? sharedLibrariesFromJson(String? libraries) {
-    if (libraries == null) return null;
+  static List<int>? sharedLibrariesFromJson(List? libraries) {
+    if (libraries == null || libraries.isEmpty) return null;
 
-    return libraries.split(';').map(int.parse).toList();
+    return libraries.map((item) => int.parse(item)).toList();
   }
 
   @override
   List<Object?> get props => [
         allowGuest,
         doNotify,
-        duration,
-        friendlyName,
-        username,
-        title,
         email,
-        guid,
-        historyRowId,
-        ipAddress,
+        filterAll,
+        filterMovies,
+        filterMusic,
+        filterPhotos,
+        filterTv,
+        friendlyName,
         isActive,
+        isAdmin,
+        isAllowSync,
+        isHomeUser,
+        isRestricted,
         keepHistory,
-        lastPlayed,
         lastSeen,
-        live,
-        mediaIndex,
-        mediaType,
-        originallyAvailableAt,
-        parentMediaIndex,
-        parentTitle,
-        platform,
-        player,
-        plays,
-        ratingKey,
         rowId,
-        thumb,
-        transcodeDecision,
-        userId,
+        sharedLibraries,
         userThumb,
-        year,
+        userId,
+        username,
       ];
 }
---
+++ lib/features/users/data/models/user_table_model.dart
@@ -0,0 +1,150 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import '../../../../core/types/types.dart';
+import '../../../../core/utilities/cast.dart';
+
+part 'user_table_model.g.dart';
+
+@JsonSerializable()
+class UserTableModel extends Equatable {
+  @JsonKey(name: 'allow_guest', fromJson: Cast.castToBool)
+  final bool? allowGuest;
+  @JsonKey(name: 'do_notify', fromJson: Cast.castToBool)
+  final bool? doNotify;
+  @JsonKey(name: 'duration', fromJson: Cast.castToInt)
+  final int? duration;
+  @JsonKey(name: 'email', fromJson: Cast.castToString)
+  final String? email;
+  @JsonKey(name: 'friendly_name', fromJson: Cast.castToString)
+  final String? friendlyName;
+  @JsonKey(name: 'guid', fromJson: Cast.castToString)
+  final String? guid;
+  @JsonKey(name: 'history_row_id', fromJson: Cast.castToInt)
+  final int? historyRowId;
+  @JsonKey(name: 'ip_address', fromJson: Cast.castToString)
+  final String? ipAddress;
+  @JsonKey(name: 'is_active', fromJson: Cast.castToBool)
+  final bool? isActive;
+  @JsonKey(name: 'keep_history', fromJson: Cast.castToBool)
+  final bool? keepHistory;
+  @JsonKey(name: 'last_played', fromJson: Cast.castToString)
+  final String? lastPlayed;
+  @JsonKey(name: 'last_seen', fromJson: Cast.castToInt)
+  final int? lastSeen;
+  @JsonKey(name: 'live', fromJson: Cast.castToBool)
+  final bool? live;
+  @JsonKey(name: 'media_index', fromJson: Cast.castToInt)
+  final int? mediaIndex;
+  @JsonKey(name: 'media_type', fromJson: Cast.castStringToMediaType)
+  final MediaType? mediaType;
+  @JsonKey(name: 'originally_available_at', fromJson: Cast.castToString)
+  final String? originallyAvailableAt;
+  @JsonKey(name: 'parent_media_index', fromJson: Cast.castToInt)
+  final int? parentMediaIndex;
+  @JsonKey(name: 'parent_title', fromJson: Cast.castToString)
+  final String? parentTitle;
+  @JsonKey(name: 'platform', fromJson: Cast.castToString)
+  final String? platform;
+  @JsonKey(name: 'player', fromJson: Cast.castToString)
+  final String? player;
+  @JsonKey(name: 'plays', fromJson: Cast.castToInt)
+  final int? plays;
+  @JsonKey(name: 'rating_key', fromJson: Cast.castToInt)
+  final int? ratingKey;
+  @JsonKey(name: 'row_id', fromJson: Cast.castToInt)
+  final int? rowId;
+  @JsonKey(name: 'thumb', fromJson: Cast.castToString)
+  final String? thumb;
+  @JsonKey(name: 'title', fromJson: Cast.castToString)
+  final String? title;
+  @JsonKey(
+      name: 'transcode_decision', fromJson: Cast.castStringToStreamDecision)
+  final StreamDecision? transcodeDecision;
+  @JsonKey(name: 'user_id', fromJson: Cast.castToInt)
+  final int? userId;
+  @JsonKey(name: 'user_thumb', fromJson: Cast.castToString)
+  final String? userThumb;
+  @JsonKey(name: 'username', fromJson: Cast.castToString)
+  final String? username;
+  @JsonKey(name: 'year', fromJson: Cast.castToInt)
+  final int? year;
+
+  const UserTableModel({
+    this.allowGuest,
+    this.doNotify,
+    this.duration,
+    this.friendlyName,
+    this.username,
+    this.title,
+    this.email,
+    this.guid,
+    this.historyRowId,
+    this.ipAddress,
+    this.isActive,
+    this.keepHistory,
+    this.lastPlayed,
+    this.lastSeen,
+    this.live,
+    this.mediaIndex,
+    this.mediaType,
+    this.originallyAvailableAt,
+    this.parentMediaIndex,
+    this.parentTitle,
+    this.platform,
+    this.player,
+    this.plays,
+    this.ratingKey,
+    this.rowId,
+    this.thumb,
+    this.transcodeDecision,
+    this.userId,
+    this.userThumb,
+    this.year,
+  });
+
+  factory UserTableModel.fromJson(Map<String, dynamic> json) =>
+      _$UserTableModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserTableModelToJson(this);
+
+  static List<int>? sharedLibrariesFromJson(String? libraries) {
+    if (libraries == null) return null;
+
+    return libraries.split(';').map(int.parse).toList();
+  }
+
+  @override
+  List<Object?> get props => [
+        allowGuest,
+        doNotify,
+        duration,
+        friendlyName,
+        username,
+        title,
+        email,
+        guid,
+        historyRowId,
+        ipAddress,
+        isActive,
+        keepHistory,
+        lastPlayed,
+        lastSeen,
+        live,
+        mediaIndex,
+        mediaType,
+        originallyAvailableAt,
+        parentMediaIndex,
+        parentTitle,
+        platform,
+        player,
+        plays,
+        ratingKey,
+        rowId,
+        thumb,
+        transcodeDecision,
+        userId,
+        userThumb,
+        year,
+      ];
+}
