595662acdcd0b07d6c238157df011c8238a66ee0
TheMeanCanEHdian <drivard100@gmail.com>
2022-08-02 13:39:48 -0500

    Add Statistics translation.

--- lib/translations/locale_keys.g.dart
+++ lib/translations/locale_keys.g.dart
@@ -278,6 +278,7 @@ abstract class  LocaleKeys {
   static const slower_title = 'slower_title';
   static const sort_users_title = 'sort_users_title';
   static const started_title = 'started_title';
+  static const statistics_title = 'statistics_title';
   static const stats_title = 'stats_title';
   static const status_title = 'status_title';
   static const stopped_title = 'stopped_title';
