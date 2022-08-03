ac5232f1a85fda8c6c94d9d792977ddd8c55e66d
TheMeanCanEHdian <drivard100@gmail.com>
2022-08-03 14:59:31 -0500

    Add inactive symbol for library icon.

--- lib/features/libraries/presentation/widgets/library_icon.dart
+++ lib/features/libraries/presentation/widgets/library_icon.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
 import '../../../../core/helpers/asset_helper.dart';
@@ -15,16 +16,47 @@ class LibraryIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (library.iconUri != null) {
-      return CachedNetworkImage(
-        imageUrl: library.iconUri.toString(),
-      );
-    } else {
-      return WebsafeSvg.asset(
-        AssetHelper.mapSectionTypeToPath(library.sectionType),
-        color: Theme.of(context).colorScheme.tertiary,
-        height: 50,
-      );
-    }
+    return Stack(
+      children: [
+        Builder(
+          builder: (context) {
+            if (library.iconUri != null) {
+              return CachedNetworkImage(
+                imageUrl: library.iconUri.toString(),
+              );
+            } else {
+              return WebsafeSvg.asset(
+                AssetHelper.mapSectionTypeToPath(library.sectionType),
+                color: Theme.of(context).colorScheme.tertiary,
+                height: 50,
+              );
+            }
+          },
+        ),
+        if (library.isActive != true)
+          Positioned(
+            bottom: -1,
+            right: 0,
+            child: Stack(
+              children: [
+                Positioned(
+                  bottom: 3,
+                  right: 5,
+                  child: FaIcon(
+                    FontAwesomeIcons.solidCircle,
+                    color: Theme.of(context).colorScheme.tertiary,
+                    size: 14,
+                  ),
+                ),
+                FaIcon(
+                  FontAwesomeIcons.triangleExclamation,
+                  color: Theme.of(context).colorScheme.secondary,
+                  size: 24,
+                ),
+              ],
+            ),
+          ),
+      ],
+    );
   }
 }
