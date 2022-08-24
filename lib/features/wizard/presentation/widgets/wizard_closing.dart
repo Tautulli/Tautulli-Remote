c7e0d2b751183a9b94ec0eb5341bca8efdf57f06
TheMeanCanEHdian <drivard100@gmail.com>
2022-08-24 13:25:49 -0500

    Only show OneSignal tile when OneSignal allowed.

--- lib/features/wizard/presentation/widgets/wizard_closing.dart
+++ lib/features/wizard/presentation/widgets/wizard_closing.dart
@@ -1,10 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
 
 import '../../../../core/widgets/notice_card.dart';
 import '../../../../translations/locale_keys.g.dart';
+import '../bloc/wizard_bloc.dart';
 import 'wizard_finish_button.dart';
 import 'wizard_previous_button.dart';
 import 'wizard_stepper.dart';
@@ -37,10 +39,24 @@ class WizardClosing extends StatelessWidget {
                     leading: const FaIcon(FontAwesomeIcons.handshakeSimple),
                     title: LocaleKeys.wizard_closing_support.tr(),
                   ),
-                  const Gap(8),
-                  NoticeCard(
-                    leading: const FaIcon(FontAwesomeIcons.solidBell),
-                    title: LocaleKeys.wizard_closing_notifications.tr(),
+                  BlocBuilder<WizardBloc, WizardState>(
+                    builder: (context, state) {
+                      state as WizardInitial;
+
+                      if (state.oneSignalAllowed) {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Gap(8),
+                            NoticeCard(
+                              leading: const FaIcon(FontAwesomeIcons.solidBell),
+                              title: LocaleKeys.wizard_closing_notifications.tr(),
+                            ),
+                          ],
+                        );
+                      }
+                      return const SizedBox(height: 0, width: 0);
+                    },
                   ),
                   const Gap(8),
                   NoticeCard(
