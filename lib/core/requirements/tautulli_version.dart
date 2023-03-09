0ce6469fb3c98069c363a49152ac4ec060bab751
TheMeanCanEHdian <drivard100@gmail.com>
2023-03-09 15:42:21 -0600

    Increase minimum required version of Tautulli.

--- lib/core/requirements/tautulli_version.dart
+++ lib/core/requirements/tautulli_version.dart
@@ -2,5 +2,5 @@ import 'package:version/version.dart';
 
 class MinimumVersion {
   /// Minimum Tautulli version required for Tautulli Remote to function.
-  static Version tautulliServer = Version(2, 6, 6);
+  static Version tautulliServer = Version(2, 10, 5);
 }
