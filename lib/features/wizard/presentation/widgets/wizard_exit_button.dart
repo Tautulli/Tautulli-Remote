ca2700eacc22a827843f7251f6634709b37c7b85
TheMeanCanEHdian <drivard100@gmail.com>
2023-08-18 19:56:11 -0500

    Fix wizard prematurely marking as complete.

--- lib/features/wizard/presentation/widgets/wizard_exit_button.dart
+++ lib/features/wizard/presentation/widgets/wizard_exit_button.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../../../../dependency_injection.dart' as di;
-import '../../../settings/domain/usecases/settings.dart';
-
 class WizardExitButton extends StatelessWidget {
   const WizardExitButton({super.key});
 
@@ -15,7 +12,6 @@ class WizardExitButton extends StatelessWidget {
       foregroundColor: Theme.of(context).colorScheme.onSurface,
       child: const FaIcon(FontAwesomeIcons.xmark),
       onPressed: () async {
-        await di.sl<Settings>().setWizardComplete(true);
         Navigator.of(context).maybePop();
       },
     );
