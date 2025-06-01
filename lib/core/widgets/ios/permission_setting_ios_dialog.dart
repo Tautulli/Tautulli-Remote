3d2b1b530f3741d7cbf068cf1d8d56fb3c92e4dd
TheMeanCanEHdian <drivard100@gmail.com>
2025-05-31 20:55:53 -0400

    Add PermissionSettingIosDialog.

---
+++ lib/core/widgets/ios/permission_setting_ios_dialog.dart
@@ -0,0 +1,45 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import '../../../translations/locale_keys.g.dart';
+
+class PermissionSettingIosDialog extends StatelessWidget {
+  final String title;
+  final String content;
+
+  const PermissionSettingIosDialog({
+    super.key,
+    required this.title,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoAlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: [
+        CupertinoDialogAction(
+          onPressed: () {
+            Navigator.of(context).pop(false);
+          },
+          child: const Text(LocaleKeys.cancel_title).tr(),
+        ),
+        CupertinoDialogAction(
+          isDefaultAction: true,
+          onPressed: () async {
+            await openAppSettings().then(
+              (value) {
+                if (value) {
+                  Navigator.of(context).pop();
+                }
+              },
+            );
+          },
+          child: const Text(LocaleKeys.go_to_settings_title).tr(),
+        ),
+      ],
+    );
+  }
+}
