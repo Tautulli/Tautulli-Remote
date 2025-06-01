871c1372d972bae854d113e33aec05b5d15e3226
TheMeanCanEHdian <drivard100@gmail.com>
2025-05-31 20:53:20 -0400

    Make headerText option for CustomCupertinoListSection.

--- lib/core/widgets/ios/custom_cupertino_list_section.dart
+++ lib/core/widgets/ios/custom_cupertino_list_section.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/cupertino.dart';
 
 class CustomCupertinoListSection extends StatelessWidget {
-  final String headerText;
+  final String? headerText;
   final List<Widget>? children;
 
   const CustomCupertinoListSection({
     super.key,
-    required this.headerText,
+    this.headerText,
     this.children,
   });
 
   @override
   Widget build(BuildContext context) {
     return CupertinoListSection.insetGrouped(
-      header: Text(headerText),
+      header: headerText != null ? Text(headerText!) : null,
       backgroundColor: CupertinoColors.transparent,
       children: children,
     );
