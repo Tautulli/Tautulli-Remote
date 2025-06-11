3f15936a6deb98e9d91e56cf5f75ecfb66943322
TheMeanCanEHdian <drivard100@gmail.com>
2025-06-11 09:21:39 -0500

    Add cutomization to CustomCupertinoListSection.

--- lib/core/widgets/ios/custom_cupertino_list_section.dart
+++ lib/core/widgets/ios/custom_cupertino_list_section.dart
@@ -3,17 +3,30 @@ import 'package:flutter/cupertino.dart';
 class CustomCupertinoListSection extends StatelessWidget {
   final String? headerText;
   final List<Widget>? children;
+  final BoxDecoration? decoration;
+  final bool hasLeading;
 
   const CustomCupertinoListSection({
     super.key,
     this.headerText,
     this.children,
+    this.decoration,
+    this.hasLeading = true,
   });
 
   @override
   Widget build(BuildContext context) {
     return CupertinoListSection.insetGrouped(
-      header: headerText != null ? Text(headerText!) : null,
+      decoration: decoration,
+      hasLeading: hasLeading,
+      header: headerText != null
+          ? Text(
+              headerText!,
+              style: TextStyle(
+                color: CupertinoTheme.of(context).primaryColor,
+              ),
+            )
+          : null,
       backgroundColor: CupertinoColors.transparent,
       children: children,
     );
