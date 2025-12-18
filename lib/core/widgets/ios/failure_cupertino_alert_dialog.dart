1244fb808592b335e88740251aed6a539b08c31a
TheMeanCanEHdian <drivard100@gmail.com>
2025-12-18 09:48:34 -0500

    Create FailureCupertinoAlertDialog.

---
+++ lib/core/widgets/ios/failure_cupertino_alert_dialog.dart
@@ -0,0 +1,29 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../../../translations/locale_keys.g.dart';
+import '../../error/failure.dart';
+import '../../helpers/failure_helper.dart';
+
+Future<void> showFailureCupertinoAlertDialog({
+  required BuildContext context,
+  required Failure failure,
+}) {
+  return showCupertinoDialog(
+    context: context,
+    builder: (context) {
+      return CupertinoAlertDialog(
+        title: Text(FailureHelper.mapFailureToMessage(failure)),
+        content: Text(FailureHelper.mapFailureToSuggestion(failure)),
+        actions: <Widget>[
+          CupertinoDialogAction(
+            child: const Text(LocaleKeys.close_title).tr(),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
