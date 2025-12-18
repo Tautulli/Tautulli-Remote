32b9f7e79e863ed18fc3a9f165d35e19e03ec5bc
TheMeanCanEHdian <drivard100@gmail.com>
2025-12-18 09:45:43 -0500

    Add cupertinoTabController.

--- lib/core/global_keys/global_keys.dart
+++ lib/core/global_keys/global_keys.dart
@@ -1,3 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+final CupertinoTabController cupertinoTabController = CupertinoTabController();
