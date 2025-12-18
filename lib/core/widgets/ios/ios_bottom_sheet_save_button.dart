e6f43196a80896431eb0c234854536ce0529fafd
TheMeanCanEHdian <drivard100@gmail.com>
2025-12-18 09:59:24 -0500

    Remove horizontal padding.

--- lib/core/widgets/ios/ios_bottom_sheet_save_button.dart
+++ lib/core/widgets/ios/ios_bottom_sheet_save_button.dart
@@ -16,7 +16,6 @@ class IosBottomSheetSaveButton extends StatelessWidget {
     return CupertinoButton(
       padding: const EdgeInsets.symmetric(
         vertical: 12,
-        horizontal: 16,
       ),
       onPressed: onPressed,
       child: const Text(LocaleKeys.save_title).tr(),
