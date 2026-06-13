f989d0ed4b00021739da9cc9a4598f79504fd32f
TheMeanCanEHdian <drivard100@gmail.com>
2026-06-13 17:34:34 -0500

    Comment out unused navBarActions for open in Plex.

--- lib/features/media/presentation/pages/material/material_style_artist_media_page.dart
+++ lib/features/media/presentation/pages/material/material_style_artist_media_page.dart
@@ -3,10 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../core/database/data/models/server_model.dart';
-import '../../../../../core/open_in_plex/open_in_plex.dart';
 import '../../../../../core/widgets/base/image_gradient_background.dart';
 import '../../../../../core/widgets/material/material_style_poster.dart';
-import '../../../../../dependency_injection.dart' as di;
 import '../../../../../translations/locale_keys.g.dart';
 import '../../../../settings/data/models/custom_header_model.dart';
 import '../../../../settings/presentation/bloc/settings_bloc.dart';
@@ -100,32 +98,32 @@ class MaterialStyleArtistMediaView extends StatelessWidget {
     );
   }
 
-  List<Widget> _appBarActions(BuildContext context) {
-    return [
-      PopupMenuButton(
-        icon: Icon(
-          Icons.more_vert,
-          color: Theme.of(context).colorScheme.onSurface,
-        ),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(12),
-          ),
-        ),
-        itemBuilder: (context) {
-          return [
-            PopupMenuItem(
-              child: const Text(LocaleKeys.view_on_plex_title).tr(),
-              onTap: () async {
-                await di.sl<OpenInPlex>().open(
-                  plexIdentifier: server.plexIdentifier,
-                  ratingKey: media.ratingKey!,
-                );
-              },
-            ),
-          ];
-        },
-      ),
-    ];
-  }
+  // List<Widget> _appBarActions(BuildContext context) {
+  //   return [
+  //     PopupMenuButton(
+  //       icon: Icon(
+  //         Icons.more_vert,
+  //         color: Theme.of(context).colorScheme.onSurface,
+  //       ),
+  //       shape: const RoundedRectangleBorder(
+  //         borderRadius: BorderRadius.all(
+  //           Radius.circular(12),
+  //         ),
+  //       ),
+  //       itemBuilder: (context) {
+  //         return [
+  //           PopupMenuItem(
+  //             child: const Text(LocaleKeys.view_on_plex_title).tr(),
+  //             onTap: () async {
+  //               await di.sl<OpenInPlex>().open(
+  //                 plexIdentifier: server.plexIdentifier,
+  //                 ratingKey: media.ratingKey!,
+  //               );
+  //             },
+  //           ),
+  //         ];
+  //       },
+  //     ),
+  //   ];
+  // }
 }
